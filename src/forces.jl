e80f44796c32c2413d06120e0016b83ab8b96d8b
Joe Greener <jgreener@hotmail.co.uk>
2020-07-29 15:23:53 +0100

    export Mie, formatting

--- src/forces.jl
+++ src/forces.jl
@@ -6,6 +6,7 @@ export
     LennardJones,
     force!,
     SoftSphere,
+    Mie,
     Coulomb,
     Gravity,
     HarmonicBond,
@@ -63,7 +64,7 @@ end
 """
     SoftSphere(nl_only)
 
-The soft-sphere potential.
+The soft-sphere interaction.
 """
 struct SoftSphere <: GeneralInteraction
     nl_only::Bool
@@ -93,42 +94,42 @@ end
 end
 
 """
-    Mie(m,n,nl_only)
+    Mie(m, n, nl_only)
 
 The Mie generalized interaction.
-Mie force with m = 6, n = 12 is equivalent to Lennard Jones potential
+When `m` equals 6 and `n` equals 12 this is equivalent to the Lennard Jones interaction.
 """
-struct Mie{T<:Real} <: GeneralInteraction
+struct Mie{T} <: GeneralInteraction
     m::T
     n::T
-    mn::T #this is to save on computation
     nl_only::Bool
+    mn_fac::T
 end
 
-Mie(m,n) = Mie(m,n,(n/(n-m))*(n/m)^(m/(n-m)),false)
+Mie(m, n, nl_only) = Mie(m, n, nl_only, convert(typeof(m), (n / (n - m)) * (n / m) ^ (m / (n - m))))
+Mie(m, n) = Mie(m, n, false)
 
 @fastmath @inbounds function force!(forces,
-    inter::Mie{T},
-    s::Simulation,
-    i::Integer,
-    j::Integer) where T
+                                    inter::Mie,
+                                    s::Simulation,
+                                    i::Integer,
+                                    j::Integer)
     if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ) || i == j
-    return
+        return
     end
     m = inter.m
     n = inter.n
     σ = sqrt(s.atoms[i].σ * s.atoms[j].σ)
     ϵ = sqrt(s.atoms[i].ϵ * s.atoms[j].ϵ)
     dr = vector(s.coords[i], s.coords[j], s.box_size)
-    rr = norm(dr)
-    abs2(rr) > sqdist_cutoff_nb && return
-    #derivative obtained via wolfram
-    const_mn = inter.mn*ϵ/rr
-    σ_r = σ/rr
-    f= m*(σ_r)^m - n*(σ_r)^n
-    f = f*const_mn
+    r = norm(dr)
+    abs2(r) > sqdist_cutoff_nb && return
+    # Derivative obtained via wolfram
+    const_mn = inter.mn_fac * ϵ / r
+    σ_r = σ / r
+    f = m * σ_r ^ m - n * σ_r ^ n
     # Limit this to 100 as a fudge to stop it exploding
-    f = min(f, 100)
+    f = min(f * const_mn, 100)
     fdr = f * dr
     forces[i] -= fdr
     forces[j] += fdr
