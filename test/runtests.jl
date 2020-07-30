68a0cce84f08586243d2ee4d49b91c905b51c422
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-07-30 23:12:08 +0300

    Add a more realistic test
    Use the parameters from https://aip.scitation.org/doi/full/10.1063/1.4726728

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,7 +92,6 @@ end
             n_steps=n_steps
         )
 
-        E0 = energy(s)
         @time simulate!(s, parallel=parallel)
 
         final_coords = last(s.loggers["coords"].coords)
@@ -101,8 +100,51 @@ end
         displacements(final_coords, box_size)
         distances(final_coords, box_size)
         rdf(final_coords, box_size)
+    end
+end
+
+@testset "Lennard-Jones gas energy conservation" begin
+    temp = 1.
+    timestep = 0.005
+    n_steps = 10_000
+    box_size = 50.0
+    n_atoms = 2000
+    m = 0.8 * box_size^3 / n_atoms
+
+    parallel_list = nthreads() > 1 ? (false, true) : (false,)
+
+    for parallel in parallel_list
+        s = Simulation(
+            simulator=VelocityVerlet(),
+            atoms=[Atom(attype="Ar", name="Ar", resnum=i, resname="Ar", charge=0.0,
+                        mass=m, σ=0.3, ϵ=0.2) for i in 1:n_atoms],
+            general_inters=(LennardJones(),),
+            coords=[box_size .* rand(SVector{3}) for i in 1:n_atoms],
+            velocities=[velocity(10.0, temp) .* 0.01 for i in 1:n_atoms],
+            temperature=temp,
+            box_size=box_size,
+            loggers=Dict("coords" => CoordinateLogger(100),
+                         "energy" => EnergyLogger(100)),
+            timestep=timestep,
+            n_steps=n_steps
+        )
+
+        E0 = energy(s)
+        @time simulate!(s, parallel=parallel)
+
         ΔE = energy(s) - E0
-        @test ΔE < 1e-2
+        @test abs(ΔE) < 1e-2
+
+        Es = s.loggers["energy"].energies
+        maxΔE = maximum(abs.(Es .- E0))
+        @test maxΔE < 1e-2
+
+        @test abs(Es[end] - Es[1]) < 1e-2
+        @test std(Es.-Es[1])/n_atoms < timestep^2
+
+        final_coords = last(s.loggers["coords"].coords)
+        @test minimum(minimum.(final_coords)) > 0.0
+        @test maximum(maximum.(final_coords)) < box_size
     end
 end
 
