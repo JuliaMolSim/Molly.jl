f05dadab430a07ea2218fa0fbcbb7bff7facd396
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-08-04 17:55:40 +0300

    Refactor Lennard-Jones
    Use the new cutoff system
    Performance optimizations via conditionally inserting code with optionally generated functions

--- src/interactions/lennard_jones.jl
+++ src/interactions/lennard_jones.jl
@@ -3,82 +3,198 @@
 
 The Lennard-Jones 6-12 interaction.
 """
-struct LennardJones{T, C} <: GeneralInteraction
+struct LennardJones{T, C, S} <: GeneralInteraction
     cutoff::C
     nl_only::Bool
     sqdist_cutoff_coeff::T
     inv_sqdist_cutoff::T
 end
 
-LennardJones() = LennardJones(ShiftCutoff(true),
-                              false,
-                              9.0,
-                              1/9.0
+LennardJones{S}(cutoff, nl_only, sqdist_cutoff_coeff=9.0) where S = 
+    LennardJones{typeof(sqdist_cutoff_coeff), typeof(cutoff), S}(
+        cutoff,
+        nl_only,
+        sqdist_cutoff_coeff,
+        inv(sqdist_cutoff_coeff)
 )
 
-LennardJones(nl_only) = LennardJones(ShiftCutoff(true),
+LennardJones(cutoff, nl_only, sqdist_cutoff_coeff=9.0) =
+    LennardJones{typeof(sqdist_cutoff_coeff), typeof(cutoff), false}(
+        cutoff,
+        nl_only,
+        sqdist_cutoff_coeff,
+        inv(sqdist_cutoff_coeff)
+)
+
+LennardJones(nl_only=false) =
+    LennardJones{Float64, ShiftedPotentialCutoff{Int}, false}(
+        ShiftedPotentialCutoff(100),
                               nl_only,
                               9.0,
                               1/9.0
 )
 
 @inline @inbounds function force!(forces,
-                                    inter::LennardJones{T, ShiftCutoff},
-                                    s::Simulation,
-                                    i::Integer,
-                                    j::Integer) where T
-    i == j && return
+                                 inter::LennardJones{T, C, S},
+                                 s::Simulation,
+                                 i::Integer,
+                                 j::Integer) where {T, C, S}
+
+    i == j && return    # TODO: get rid of this check
     dr = vector(s.coords[i], s.coords[j], s.box_size)
     r2 = sum(abs2, dr)
+
+    if @generated
+        if !S
+            quote
+                if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
+                    return
+                end
+            end
+        end
+    else
+        if !S && iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
+            return
+        end
+    end
 
     σ = sqrt(s.atoms[i].σ * s.atoms[j].σ)
     ϵ = sqrt(s.atoms[i].ϵ * s.atoms[j].ϵ)
 
     σ2 = σ^2
-    sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
-    r2 > sqdist_cutoff && return
-    
-    invr2 = inv(r2)
-    six_term = (σ2 * invr2) ^ 3
-    f = (24ϵ * invr2) * (2 * six_term ^ 2 - six_term)
-    if inter.cutoff.limit_force
-        # Limit this to 100 as a fudge to stop it exploding
-        f = min(f, 100)
+
+    if @generated
+        if cutoff_points(C) == 1
+            quote
+                sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+                r2 > sqdist_cutoff && return
+
+                f = force_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+            end
+        elseif cutoff_points(C) == 2
+            quote
+                sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+                activation_dist = inter.activation_dist_coeff * σ2
+
+                r2 > sqdist_cutoff && return
+
+                if r2 < activation_dist
+                    f = force(inter, inv(r2), (σ2, ϵ))
+                else
+                    f = force_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+                end
+            end
+        end
+    else
+        if cutoff_points(C) == 1
+            sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+            r2 > sqdist_cutoff && return
+
+            f = force_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+        elseif cutoff_points(C) == 2
+            sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+            activation_dist = inter.activation_dist_coeff * σ2
+
+            r2 > sqdist_cutoff && return
+            
+            if r2 < activation_dist
+                f = force(inter, inv(r2), (σ2, ϵ))
+            else
+                f = force_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+            end
+        end
     end
+
     fdr = f * dr
     forces[i] -= fdr
     forces[j] += fdr
     return nothing
 end
 
-@inbounds function potential_energy(inter::LennardJones,
-                                    s::Simulation,
-                                    i::Integer,
-                                    j::Integer)
-    T = eltype(s.coords[i]) # this is not Unitful compatible
+@fastmath function force(::LennardJones, invr2, (σ2, ϵ))
+    six_term = (σ2 * invr2) ^ 3
+    
+    return (24ϵ * invr2) * (2 * six_term ^ 2 - six_term)
+end
+
+@inline @inbounds function potential_energy(inter::LennardJones{T, C, S},
+                                            s::Simulation,
+                                            i::Integer,
+                                            j::Integer) where {T, C, S}
+    U = eltype(s.coords[i]) # this is not Unitful compatible
     i == j && return zero(T)
+
     dr = vector(s.coords[i], s.coords[j], s.box_size)
     r2 = sum(abs2, dr)
-    
-    if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
-        return zero(T)
+
+    if @generated
+        if !S
+            quote
+                if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
+                    return zero(U)
+                end
+            end
+        end
+    else
+        if !S && iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
+            return zero(U)
+        end
     end
+
     σ = sqrt(s.atoms[i].σ * s.atoms[j].σ)
     ϵ = sqrt(s.atoms[i].ϵ * s.atoms[j].ϵ)
     
     σ2 = σ^2
-    sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
-    r2 > sqdist_cutoff && return zero(T)
-    
-    invr2 = inv(r2)
-    six_term = (σ2 * invr2) ^ 3
 
-    return 4ϵ * (six_term ^ 2 - six_term) - cutoff_energy(inter, σ, ϵ)
+    if @generated
+        if cutoff_points(C) == 1
+            quote
+                sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+                r2 > sqdist_cutoff && return zero(U)
+
+                potential_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+            end
+        elseif cutoff_points(C) == 2
+            quote
+                r2 > sqdist_cutoff && return zero(U)
+            
+                if r2 < activation_dist
+                    potential(inter, inv(r2), (σ2, ϵ))
+                else
+                    potential_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+                end
+            end
+        end
+    else
+        if cutoff_points(C) == 1
+            sqdist_cutoff = inter.sqdist_cutoff_coeff * σ2
+            r2 > sqdist_cutoff && return zero(U)
+
+            potential_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+        elseif cutoff_points(C) == 2
+            r2 > sqdist_cutoff && return zero(U)
+            
+            if r2 < activation_dist
+                potential(inter, inv(r2), (σ2, ϵ))
+            else
+                potential_cutoff(inter.cutoff, r2, inter, (σ2, ϵ))
+            end
+        end
+    end
 end
 
-function cutoff_energy(inter::LennardJones, σ, ϵ)
-    invr2 = inter.inv_sqdist_cutoff
-    six_term = (σ ^ 2 * invr2) ^ 3
+@fastmath function potential(::LennardJones, invr2, (σ2, ϵ))
+    six_term = (σ2 * invr2) ^ 3
     
-    4ϵ * (six_term ^ 2 - six_term)
+    return 4ϵ * (six_term ^ 2 - six_term)
+end
+
+@fastmath function potential_cutoff(::ShiftedForceCutoff{false}, r2, inter::LennardJones, (σ2, ϵ))
+    invr2 = inv(r2)
+    r = √r2
+    rc = √(inter.sqdist_cutoff_coeff * σ2)
+    fc = force(inter, inter.inv_sqdist_cutoff, (σ2, ϵ))
+
+    potential(inter, invr2, (σ2, ϵ)) - (r - rc) * fc -
+        potential(inter, inter.inv_sqdist_cutoff, (σ2, ϵ))
 end
