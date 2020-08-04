b4e073ebd20a803056ed6b85d3b8a823babdc777
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-08-04 17:53:55 +0300

    Redesign cutoffs

--- src/cutoffs.jl
+++ src/cutoffs.jl
@@ -1,5 +1,67 @@
-export ShiftCutoff
+export ShiftedPotentialCutoff, ShiftedForceCutoff, NoCutoff
 
-struct ShiftCutoff <: AbstractCutoff
-    limit_force::Bool
+struct NoCutoff <: AbstractCutoff end
+
+cutoff_points(::Type{NoCutoff}) = 0
+
+struct ShiftedPotentialCutoff{T} <: AbstractCutoff
+    min_force::T
 end
+
+cutoff_points(::Type{ShiftedPotentialCutoff{T}}) where T = 1
+
+struct ShiftedForceCutoff{L, T} <: AbstractCutoff
+    min_force::T
+end
+
+cutoff_points(::Type{ShiftedForceCutoff}) = 1
+
+function force_cutoff(cutoff::ShiftedPotentialCutoff{T}, r2, inter, params) where T
+    invr2 = inv(r2)
+    f = force(inter, invr2, params)
+
+    if @generated
+        quote
+            if !(T === Nothing)
+                f = min(f, cutoff.min_force)
+            end
+        end
+    else
+        if !(T === Nothing)
+            f = min(f, cutoff.min_force)
+        end
+    end
+
+    return f
+end
+
+function potential_cutoff(::ShiftedPotentialCutoff, r2, inter, params)
+    invr2 = inv(r2)
+    potential(inter, invr2, params) - potential(inter, inter.inv_sqdist_cutoff, params)
+end
+
+function force_cutoff(::ShiftedForceCutoff{T}, r2, inter, params) where T
+    invr2 = inv(r2)
+    f = force(inter, invr2, params) - force(inter, inter.inv_sqdist_cutoff, params)
+
+    if @generated
+        quote
+            if !(T === Nothing)
+                f = min(f, cutoff.min_force)
+            end
+        end
+    else
+        if !(T === Nothing)
+            f = min(f, cutoff.min_force)
+        end
+    end
+
+    return f
+end
+
+function potential_cutoff(::ShiftedForceCutoff{false}, r2, inter, params)
+    # implement generic fallback
+end
+
+force_cutoff(::NoCutoff, r2, inter, params) = force(inter, inv(r2), params)
+potential_cutoff(::NoCutoff, r2, inter, params) = potential(inter, inv(r2), params)
