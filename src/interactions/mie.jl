8c9fbca4fbc0921554f19e0478eeb59488934609
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-08-11 22:37:10 +0300

    Refactor Mie interaction

--- src/interactions/mie.jl
+++ src/interactions/mie.jl
@@ -19,24 +19,98 @@ Mie(m, n) = Mie(m, n, false)
                                     s::Simulation,
                                     i::Integer,
                                     j::Integer)
-    if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ) || i == j
+    i == j && return    # TODO: get rid of this check
+    dr = vector(s.coords[i], s.coords[j], s.box_size)
+    r2 = sum(abs2, dr)
+    r = √r2
+
+    if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
         return
     end
-    m = inter.m
-    n = inter.n
+
     σ = sqrt(s.atoms[i].σ * s.atoms[j].σ)
     ϵ = sqrt(s.atoms[i].ϵ * s.atoms[j].ϵ)
-    dr = vector(s.coords[i], s.coords[j], s.box_size)
-    r = norm(dr)
-    abs2(r) > sqdist_cutoff_nb && return
+    
+    cutoff = inter.cutoff
+    m = inter.m
+    n = inter.n
     # Derivative obtained via wolfram
-    const_mn = inter.mn_fac * ϵ / r
+    const_mn = inter.mn_fac * ϵ
     σ_r = σ / r
-    f = m * σ_r ^ m - n * σ_r ^ n
-    # Limit this to 100 as a fudge to stop it exploding
-    f = min(-f * const_mn / r, 100)
+    params = (m, n, σ_r, const_mn)
+
+    if cutoff_points(C) == 0
+        f = force(inter, r2, inv(r2), params)
+    elseif cutoff_points(C) == 1
+        sqdist_cutoff = cutoff.sqdist_cutoff * σ2
+        r2 > sqdist_cutoff && return
+
+        f = force_cutoff(cutoff, r2, inter, params)
+    elseif cutoff_points(C) == 2
+        sqdist_cutoff = cutoff.sqdist_cutoff * σ2
+        activation_dist = cutoff.activation_dist * σ2
+
+        r2 > sqdist_cutoff && return
+        
+        if r2 < activation_dist
+            f = force(inter, r2, inv(r2), params)
+        else
+            f = force_cutoff(cutoff, r2, inter, params)
+        end
+    end
+
     fdr = f * dr
     forces[i] -= fdr
     forces[j] += fdr
     return nothing
+end
+
+@fastmath function force(::Mie, r2, invr2, (m, n, σ_r, const_mn))
+    return -const_mn / r2 * (m * σ_r ^ m - n * σ_r ^ n)
+end
+
+@inline @inbounds function potential_energy(inter::Mie,
+                                            s::Simulation,
+                                            i::Integer,
+                                            j::Integer)
+    U = eltype(s.coords[i]) # this is not Unitful compatible
+    i == j && return zero(T)
+
+    dr = vector(s.coords[i], s.coords[j], s.box_size)
+    r2 = sum(abs2, dr)
+
+    if iszero(s.atoms[i].σ) || iszero(s.atoms[j].σ)
+        return zero(U)
+    end
+
+    σ = sqrt(s.atoms[i].σ * s.atoms[j].σ)
+    ϵ = sqrt(s.atoms[i].ϵ * s.atoms[j].ϵ)
+    
+    cutoff = inter.cutoff
+    m = inter.m
+    n = inter.n
+    const_mn = inter.mn_fac * ϵ
+    σ_r = σ / r
+    params = (m, n, σ_r, const_mn)
+
+    if cutoff_points(C) == 0
+        potential(inter, r2, inv(r2), params)
+    elseif cutoff_points(C) == 1
+        sqdist_cutoff = cutoff.sqdist_cutoff * σ2
+        r2 > sqdist_cutoff && return zero(U)
+
+        potential_cutoff(cutoff, r2, inter, params)
+    elseif cutoff_points(C) == 2
+        r2 > sqdist_cutoff && return zero(U)
+        
+        if r2 < activation_dist
+            potential(inter, r2, inv(r2), params)
+        else
+            potential_cutoff(cutoff, r2, inter, params)
+        end
+    end
+end
+
+@fastmath function potential(::Mie, r2, invr2, (m, n, σ_r, const_mn))
+    return const_mn * (σ_r ^ m - σ_r ^ m)
 end
