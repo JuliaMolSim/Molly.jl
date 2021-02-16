24f182df8e8067fdc730e5bf139663469bbd87aa
Maximilian Scheurer <max.scheurer@me.com>
2021-02-16 15:18:29 +0100

    tree search neighbors

--- src/neighbours.jl
+++ src/neighbours.jl
@@ -3,7 +3,8 @@
 export
     NoNeighbourFinder,
     find_neighbours!,
-    DistanceNeighbourFinder
+    DistanceNeighbourFinder,
+    DistanceNeighbourFinderTree
 
 """
     NoNeighbourFinder()
@@ -79,6 +80,68 @@ function find_neighbours!(s::Simulation,
             for j in 1:(i - 1)
                 r2 = sum(abs2, vector(ci, s.coords[j], s.box_size))
                 if r2 <= sqdist_cutoff && nbi[j]
+                    push!(neighbours, (i, j))
+                end
+            end
+        end
+    end
+end
+
+
+"""
+    DistanceNeighbourFinderTree(nb_matrix, n_steps, dist_cutoff)
+    DistanceNeighbourFinderTree(nb_matrix, n_steps)
+
+Find close atoms by distance (using a tree search).
+"""
+struct DistanceNeighbourFinderTree{T} <: NeighbourFinder
+    nb_matrix::BitArray{2}
+    n_steps::Int
+    dist_cutoff::T
+end
+
+function DistanceNeighbourFinderTree(nb_matrix::BitArray{2},
+                                 n_steps::Integer)
+    return DistanceNeighbourFinderTree(nb_matrix, n_steps, 1.2)
+end
+
+function find_neighbours!(s::Simulation,
+                          nf::DistanceNeighbourFinderTree,
+                          step_n::Integer;
+                          parallel::Bool=true)
+    !iszero(step_n % nf.n_steps) && return
+
+    neighbours = s.neighbours
+    empty!(neighbours)
+
+    bv = SVector{3}(s.box_size, s.box_size, s.box_size)
+    btree = BallTree(s.coords, PeriodicEuclidean(bv))
+
+    if parallel && nthreads() > 1
+        nl_threads = [Tuple{Int, Int}[] for i in 1:nthreads()]
+
+        @threads for i in 1:length(s.coords)
+            nl = nl_threads[threadid()]
+            ci = s.coords[i]
+            nbi = nf.nb_matrix[:, i]
+            idxs = inrange(btree, ci, nf.dist_cutoff, true)
+            for j in idxs
+                if nbi[j] && i > j
+                    push!(nl, (i, j))
+                end
+            end
+        end
+
+        for nl in nl_threads
+            append!(neighbours, nl)
+        end
+    else
+        for i in 1:length(s.coords)
+            ci = s.coords[i]
+            nbi = nf.nb_matrix[:, i]
+            idxs = inrange(btree, ci, nf.dist_cutoff, true)
+            for j in idxs
+                if nbi[j] && i > j
                     push!(neighbours, (i, j))
                 end
             end
