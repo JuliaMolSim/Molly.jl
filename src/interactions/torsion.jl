1bf9d315880271a11ec63268af7c6c952d0a334d
Joe Greener <jgreener@hotmail.co.uk>
2021-05-18 23:51:19 +0100

    better dihedral force calculation

--- src/interactions/torsion.jl
+++ src/interactions/torsion.jl
@@ -17,30 +17,31 @@ end
 @inline @inbounds function force(d::Torsion,
                                   coords,
                                   s::Simulation)
-    ba = vector(coords[d.j], coords[d.i], s.box_size)
+    ab = vector(coords[d.i], coords[d.j], s.box_size)
     bc = vector(coords[d.j], coords[d.k], s.box_size)
-    dc = vector(coords[d.l], coords[d.k], s.box_size)
-    p1 = normalize(ba × bc)
-    p2 = normalize(-dc × -bc)
-    θ = atan(dot((-ba × bc) × (bc × -dc), normalize(bc)), dot(-ba × bc, bc × -dc))
+    cd = vector(coords[d.k], coords[d.l], s.box_size)
+    cross_ab_bc = ab × bc
+    cross_bc_cd = bc × cd
+    θ = atan(dot(cross_ab_bc × cross_bc_cd, normalize(bc)), dot(cross_ab_bc, cross_bc_cd))
     angle_term = (d.f1*sin(θ) - 2*d.f2*sin(2*θ) + 3*d.f3*sin(3*θ)) / 2
-    fa = (angle_term / (norm(ba) * sin(acosbound(dot(ba, bc) / (norm(ba) * norm(bc)))))) * p1
+    fa = angle_term * normalize(-cross_ab_bc) / norm(ab)
     # fd clashes with a function name
-    f_d = (angle_term / (norm(dc) * sin(acosbound(dot(bc, dc) / (norm(bc) * norm(dc)))))) * p2
-    oc = bc / 2
-    tc = -(oc × f_d + (-dc × f_d) / 2 + (ba × fa) / 2)
-    fc = (1 / dot(oc, oc)) * (tc × oc)
-    fb = -fa - fc - f_d
+    f_d = angle_term * normalize(cross_bc_cd) / norm(cd)
+    # Forces on the middle atoms have to keep the sum of torques null
+    # Forces taken from http://www.softberry.com/freedownloadhelp/moldyn/description.html
+    bc_norm = norm(bc)
+    fb = (((ab .* -bc) / (bc_norm ^ 2)) .- 1) .* fa .- ((cd .* -bc) / (bc_norm ^ 2)) .* f_d
+    fc = -fa - fb - f_d
     return [d.i, d.j, d.k, d.l], [fa, fb, fc, f_d]
 end
 
 @inline @inbounds function potential_energy(d::Torsion,
                                             s::Simulation)
-    ba = vector(s.coords[d.j], s.coords[d.i], s.box_size)
+    ab = vector(s.coords[d.i], s.coords[d.j], s.box_size)
     bc = vector(s.coords[d.j], s.coords[d.k], s.box_size)
-    dc = vector(s.coords[d.l], s.coords[d.k], s.box_size)
-
-    θ = atan(dot((-ba × bc) × (bc × -dc), normalize(bc)), dot(-ba × bc, bc × -dc))
-
-    (d.f1 * (1 + cos(θ)) + d.f2 * (1 - cos(2θ)) + d.f3 * (1 + cos(3θ)) + d.f4) / 2
+    cd = vector(s.coords[d.k], s.coords[d.l], s.box_size)
+    cross_ab_bc = ab × bc
+    cross_bc_cd = bc × cd
+    θ = atan(dot(cross_ab_bc × cross_bc_cd, normalize(bc)), dot(cross_ab_bc, cross_bc_cd))
+    return (d.f1 * (1 + cos(θ)) + d.f2 * (1 - cos(2θ)) + d.f3 * (1 + cos(3θ)) + d.f4) / 2
 end
