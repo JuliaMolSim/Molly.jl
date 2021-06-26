ae4b5340e3dc3b0e0ae6cc6ab6ec6285391b920b
Joe Greener <jgreener@hotmail.co.uk>
2021-06-26 15:38:36 +0100

    units in rdf function

--- src/analysis.jl
+++ src/analysis.jl
@@ -62,13 +62,17 @@ This describes how density varies as a function of distance from each atom.
 Returns a list of distance bin centres and a list of the corresponding
 densities.
 """
-function rdf(coords, box_size::Real; npoints::Integer=200)
+function rdf(coords, box_size; npoints::Integer=200)
     n_atoms = length(coords)
     dists = distances(coords, box_size)
     dists_vec = [dists[i, j] for i in 1:n_atoms, j in 1:n_atoms if j > i]
-    kd = kde(dists_vec, npoints=npoints)
-    density_weighted = kd.density ./ (4π .* step(kd.x) .* kd.x .^ 2)
-    return collect(kd.x), density_weighted
+    dist_unit = unit(first(dists_vec))
+    kd = kde(ustrip.(dists_vec), npoints=npoints)
+    ρ = n_atoms / box_size ^ 3
+    normalizing_factor = 4π .* ρ .* step(kd.x) .* kd.x .^ 2 .* dist_unit .^ 3
+    bin_centres = collect(kd.x) .* dist_unit
+    density_weighted = kd.density ./ normalizing_factor
+    return bin_centres, density_weighted
 end
 
 """
