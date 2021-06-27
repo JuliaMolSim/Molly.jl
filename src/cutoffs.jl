53d5d3234f1ee1bb3dce4c1c4bbff995150efe3d
Joe Greener <jgreener@hotmail.co.uk>
2021-06-27 18:59:34 +0100

    allow unit type parameters in cutoff types

--- src/cutoffs.jl
+++ src/cutoffs.jl
@@ -1,40 +1,54 @@
-export ShiftedPotentialCutoff, ShiftedForceCutoff, NoCutoff
+# Cutoff strategies for long-range interactions
 
+export
+    NoCutoff,
+    ShiftedPotentialCutoff,
+    ShiftedForceCutoff
+
+"""
+    NoCutoff()
+
+Placeholder cutoff that does not alter forces or potentials.
+"""
 struct NoCutoff <: AbstractCutoff end
 
 cutoff_points(::Type{NoCutoff}) = 0
 
-struct ShiftedPotentialCutoff{F, T} <: AbstractCutoff
+"""
+    ShiftedPotentialCutoff(; cutoff_dist, max_force)
+
+Cutoff that shifts the potential to be continuous at a specified cutoff point.
+"""
+struct ShiftedPotentialCutoff{F, D, S, I} <: AbstractCutoff
     max_force::F
-    cutoff_dist::T
-    sqdist_cutoff::T
-    inv_sqdist_cutoff::T
+    cutoff_dist::D
+    sqdist_cutoff::S
+    inv_sqdist_cutoff::I
 end
 
-ShiftedPotentialCutoff(cutoff_dist) = ShiftedPotentialCutoff(
-    100,
-    cutoff_dist,
-    cutoff_dist^2,
-    inv(cutoff_dist),
-)
-
-cutoff_points(::Type{ShiftedPotentialCutoff{F, T}}) where {F, T} = 1
-
-struct ShiftedForceCutoff{F, T} <: AbstractCutoff
-    max_force::F
-    cutoff_dist::T
-    sqdist_cutoff::T
-    inv_sqdist_cutoff::T
+function ShiftedPotentialCutoff(; cutoff_dist, max_force=nothing)
+    return ShiftedPotentialCutoff(max_force, cutoff_dist, cutoff_dist ^ 2, inv(cutoff_dist))
 end
 
-ShiftedForceCutoff(cutoff_dist) = ShiftedForceCutoff(
-    100,
-    cutoff_dist,
-    cutoff_dist^2,
-    inv(cutoff_dist),
-)
+cutoff_points(::Type{ShiftedPotentialCutoff{F, D, S, I}}) where {F, D, S, I} = 1
 
-cutoff_points(::Type{ShiftedForceCutoff{F, T}}) where {F, T} = 1
+"""
+    ShiftedForceCutoff(; cutoff_dist, max_force)
+
+Cutoff that shifts the force to be continuous at a specified cutoff point.
+"""
+struct ShiftedForceCutoff{F, D, S, I} <: AbstractCutoff
+    max_force::F
+    cutoff_dist::D
+    sqdist_cutoff::S
+    inv_sqdist_cutoff::I
+end
+
+function ShiftedForceCutoff(; cutoff_dist, max_force=nothing)
+    return ShiftedForceCutoff(max_force, cutoff_dist, cutoff_dist ^ 2, inv(cutoff_dist))
+end
+
+cutoff_points(::Type{ShiftedForceCutoff{F, D, S, I}}) where {F, D, S, I} = 1
 
 force_cutoff(::NoCutoff, r2, inter, params) = force_nocutoff(inter, r2, inv(r2), params)
 potential_cutoff(::NoCutoff, r2, inter, params) = potential(inter, r2, inv(r2), params)
