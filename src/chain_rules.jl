c439229f16eb013ecfe59c8fffd35dec1194bb74
Joe Greener <jgreener@hotmail.co.uk>
2021-11-08 16:20:25 +0000

    SVector constructor rrule, basic gradients work

--- src/chain_rules.jl
+++ src/chain_rules.jl
@@ -1,5 +1,12 @@
 # Chain rules to allow differentiable simulations
 
+function ChainRulesCore.rrule(T::Type{<:SVector}, x::Number...)
+    function SVector_pullback(Ȳ)
+        return NoTangent(), Ȳ...
+    end
+    return T(x...), SVector_pullback
+end
+
 function ChainRulesCore.rrule(::typeof(accumulateadd), x)
     Y = accumulateadd(x)
     function accumulateadd_pullback(Ȳ)
