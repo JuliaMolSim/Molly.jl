c8f3da5a03680752f10b91105b3e695daab7b420
chemicalfiend <teamkagaku101@gmail.com>
2022-02-15 17:30:57 +0530

    Added Morse potential

---
+++ src/interactions/morse.jl
@@ -0,0 +1,32 @@
+export Morse
+
+"""
+    Morse
+
+Morse potential for bond between two atoms
+
+"""
+
+struct Morse{D, A, R} <: SpecificInteraction
+    depth::D
+    α::A
+    r0::R
+end
+
+
+@inline @inbounds function force(b::Morse, coord_i, coord_j, box_size)
+    dr = vector(coord_i, coord_j, box_size)
+    r = norm(dr)
+    ralp = exp(-b.α*(r-b.r0))
+    c = -2.0*b.D*b.α*(1-ralp)*ralp/r
+    f = c * normalize(dr)
+    return SpecificForce2Atoms(f, -f)
+end
+
+@inline @inbounds function potential_energy(b::Morse, coord_i, coord_j, box_size)
+    dr = vector(coord_i, coord_j, box_size)
+    r = norm(dr)
+    ralp = exp(-b.α*(r - b.r0))
+    return b.D * (1 - ralp)^2
+end
+
