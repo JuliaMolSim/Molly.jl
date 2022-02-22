da66ac133bf205983a7f076e917c780b0a820aee
Joe Greener <jgreener@hotmail.co.uk>
2022-02-22 18:12:12 +0000

    single simulator function

--- src/simulators.jl
+++ src/simulators.jl
@@ -38,12 +38,10 @@ VelocityVerlet(; dt, coupling=NoCoupling()) = VelocityVerlet(dt, coupling)
 Run a simulation on a system according to the rules of the given simulator.
 Custom simulators should implement this function.
 """
-function simulate!(sys::System{D, false},
+function simulate!(sys,
                     sim::VelocityVerlet,
                     n_steps::Integer;
-                    parallel::Bool=true) where D
-    # See https://www.saylor.org/site/wp-content/uploads/2011/06/MA221-6.1.pdf for
-    #   integration algorithm - used shorter second version
+                    parallel::Bool=true)
     neighbors = find_neighbors(sys, sys.neighbor_finder; parallel=parallel)
     accels_t = accelerations(sys, neighbors; parallel=parallel)
     accels_t_dt = zero(accels_t)
@@ -51,49 +49,16 @@ function simulate!(sys::System{D, false},
     for step_n in 1:n_steps
         run_loggers!(sys, neighbors, step_n)
 
-        # Update coordinates
-        for i in 1:length(sys)
-            sys.coords[i] += sys.velocities[i] * sim.dt + remove_molar(accels_t[i]) * (sim.dt ^ 2) / 2
-            sys.coords[i] = wrap_coords.(sys.coords[i], sys.box_size)
-        end
-
-        accels_t_dt = accelerations(sys, neighbors; parallel=parallel)
-
-        # Update velocities
-        for i in 1:length(sys)
-            sys.velocities[i] += remove_molar(accels_t[i] + accels_t_dt[i]) * sim.dt / 2
-        end
-
-        apply_coupling!(sys, sim, sim.coupling)
-
-        if step_n != n_steps
-            neighbors = find_neighbors(sys, sys.neighbor_finder, neighbors, step_n; parallel=parallel)
-            accels_t = accels_t_dt
-        end
-    end
-    return sys
-end
-
-function simulate!(sys::System{D, true},
-                    sim::VelocityVerlet,
-                    n_steps::Integer;
-                    parallel::Bool=true) where D
-    neighbors = find_neighbors(sys, sys.neighbor_finder)
-    accels_t = accelerations(sys, neighbors)
-    accels_t_dt = zero(accels_t)
-
-    for step_n in 1:n_steps
-        run_loggers!(sys, neighbors, step_n)
-
         sys.coords += sys.velocities .* sim.dt .+ (remove_molar.(accels_t) .* sim.dt ^ 2) ./ 2
         sys.coords = wrap_coords_vec.(sys.coords, (sys.box_size,))
-        accels_t_dt = accelerations(sys, neighbors)
+        accels_t_dt = accelerations(sys, neighbors; parallel=parallel)
         sys.velocities += remove_molar.(accels_t .+ accels_t_dt) .* sim.dt / 2
 
         apply_coupling!(sys, sim, sim.coupling)
 
         if step_n != n_steps
-            neighbors = find_neighbors(sys, sys.neighbor_finder, neighbors, step_n)
+            neighbors = find_neighbors(sys, sys.neighbor_finder, neighbors, step_n;
+                                        parallel=parallel)
             accels_t = accels_t_dt
         end
     end
