f3c36350e0feda3ed595f4c989dcb407fd8db9ec
Noé Blassel <noe.blassel@gmail.com>
2022-06-22 12:41:53 +0100

    add TimeCorrelationLogger and DataStructures dependency

--- src/loggers.jl
+++ src/loggers.jl
@@ -10,7 +10,9 @@ export
     KineticEnergyLogger,
     PotentialEnergyLogger,
     ForceLogger,
-    StructureWriter
+    StructureWriter,
+    TimeCorrelationLogger,
+    compute_statistical_correlation
 
 """
     run_loggers!(system, neighbors=nothing, step_n=0; parallel=true)
@@ -40,8 +42,8 @@ TemperatureLogger(n_steps::Integer) = TemperatureLogger(typeof(one(DefaultFloat)
 
 function Base.show(io::IO, tl::TemperatureLogger)
     print(io, "TemperatureLogger{", eltype(tl.temperatures), "} with n_steps ",
-                tl.n_steps, ", ", length(tl.temperatures),
-                " temperatures recorded")
+        tl.n_steps, ", ", length(tl.temperatures),
+        " temperatures recorded")
 end
 
 """
@@ -51,7 +53,7 @@ Log a property of the system thoughout a simulation.
 Custom loggers should implement this function.
 """
 function log_property!(logger::TemperatureLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.temperatures, temperature(s))
     end
@@ -70,7 +72,7 @@ end
 
 function CoordinateLogger(T, n_steps::Integer; dims::Integer=3)
     return CoordinateLogger(n_steps,
-                            Array{SArray{Tuple{dims}, T, 1, dims}, 1}[])
+        Array{SArray{Tuple{dims},T,1,dims},1}[])
 end
 
 function CoordinateLogger(n_steps::Integer; dims::Integer=3)
@@ -79,12 +81,12 @@ end
 
 function Base.show(io::IO, cl::CoordinateLogger)
     print(io, "CoordinateLogger{", eltype(eltype(cl.coords)), "} with n_steps ",
-            cl.n_steps, ", ", length(cl.coords), " frames recorded for ",
-            length(cl.coords) > 0 ? length(first(cl.coords)) : "?", " atoms")
+        cl.n_steps, ", ", length(cl.coords), " frames recorded for ",
+        length(cl.coords) > 0 ? length(first(cl.coords)) : "?", " atoms")
 end
 
 function log_property!(logger::CoordinateLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.coords, deepcopy(s.coords))
     end
@@ -103,7 +105,7 @@ end
 
 function VelocityLogger(T, n_steps::Integer; dims::Integer=3)
     return VelocityLogger(n_steps,
-                            Array{SArray{Tuple{dims}, T, 1, dims}, 1}[])
+        Array{SArray{Tuple{dims},T,1,dims},1}[])
 end
 
 function VelocityLogger(n_steps::Integer; dims::Integer=3)
@@ -112,12 +114,12 @@ end
 
 function Base.show(io::IO, vl::VelocityLogger)
     print(io, "VelocityLogger{", eltype(eltype(vl.velocities)), "} with n_steps ",
-            vl.n_steps, ", ", length(vl.velocities), " frames recorded for ",
-            length(vl.velocities) > 0 ? length(first(vl.velocities)) : "?", " atoms")
+        vl.n_steps, ", ", length(vl.velocities), " frames recorded for ",
+        length(vl.velocities) > 0 ? length(first(vl.velocities)) : "?", " atoms")
 end
 
 function log_property!(logger::VelocityLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.velocities, deepcopy(s.velocities))
     end
@@ -142,11 +144,11 @@ end
 
 function Base.show(io::IO, el::TotalEnergyLogger)
     print(io, "TotalEnergyLogger{", eltype(el.energies), "} with n_steps ",
-                el.n_steps, ", ", length(el.energies), " energies recorded")
+        el.n_steps, ", ", length(el.energies), " energies recorded")
 end
 
 function log_property!(logger::TotalEnergyLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.energies, total_energy(s, neighbors))
     end
@@ -171,11 +173,11 @@ end
 
 function Base.show(io::IO, el::KineticEnergyLogger)
     print(io, "KineticEnergyLogger{", eltype(el.energies), "} with n_steps ",
-                el.n_steps, ", ", length(el.energies), " energies recorded")
+        el.n_steps, ", ", length(el.energies), " energies recorded")
 end
 
 function log_property!(logger::KineticEnergyLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.energies, kinetic_energy(s))
     end
@@ -200,11 +202,11 @@ end
 
 function Base.show(io::IO, el::PotentialEnergyLogger)
     print(io, "PotentialEnergyLogger{", eltype(el.energies), "} with n_steps ",
-                el.n_steps, ", ", length(el.energies), " energies recorded")
+        el.n_steps, ", ", length(el.energies), " energies recorded")
 end
 
 function log_property!(logger::PotentialEnergyLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         push!(logger.energies, potential_energy(s, neighbors))
     end
@@ -223,7 +225,7 @@ end
 
 function ForceLogger(T, n_steps::Integer; dims::Integer=3)
     return ForceLogger(n_steps,
-                        Array{SArray{Tuple{dims}, T, 1, dims}, 1}[])
+        Array{SArray{Tuple{dims},T,1,dims},1}[])
 end
 
 function ForceLogger(n_steps::Integer; dims::Integer=3)
@@ -232,12 +234,12 @@ end
 
 function Base.show(io::IO, fl::ForceLogger)
     print(io, "ForceLogger{", eltype(eltype(fl.forces)), "} with n_steps ",
-            fl.n_steps, ", ", length(fl.forces), " frames recorded for ",
-            length(fl.forces) > 0 ? length(first(fl.forces)) : "?", " atoms")
+        fl.n_steps, ", ", length(fl.forces), " frames recorded for ",
+        length(fl.forces) > 0 ? length(first(fl.forces)) : "?", " atoms")
 end
 
 function log_property!(logger::ForceLogger, s::System, neighbors=nothing,
-                        step_n::Integer=0; parallel::Bool=true)
+    step_n::Integer=0; parallel::Bool=true)
     if step_n % logger.n_steps == 0
         push!(logger.forces, forces(s, neighbors; parallel=parallel))
     end
@@ -261,15 +263,15 @@ end
 
 function Base.show(io::IO, sw::StructureWriter)
     print(io, "StructureWriter with n_steps ", sw.n_steps, ", filepath \"",
-                sw.filepath, "\", ", sw.structure_n, " frames written")
+        sw.filepath, "\", ", sw.structure_n, " frames written")
 end
 
 function log_property!(logger::StructureWriter, s::System, neighbors=nothing,
-                        step_n::Integer=0; kwargs...)
+    step_n::Integer=0; kwargs...)
     if step_n % logger.n_steps == 0
         if length(s) != length(s.atoms_data)
             error("Number of atoms is ", length(s), " but number of atom data entries is ",
-                    length(s.atoms_data))
+                length(s.atoms_data))
         end
         append_model!(logger, s)
     end
@@ -304,13 +306,12 @@ atom_record(at_data, i, coord) = BioStructures.AtomRecord(
 
 """
 A time correlation logger, which allow to estimate statistical correlations of the form
-\[C(t)=\left(\left\langle A(t)\cdot B(0)\right\rangle -\left\langle A(0)\cdot B(0)\right\rangle\right)\left(\sqrt{\left\langle |A|^2\right\rangle\left\langle |B|^2\right\rangle}\right)^{-1}\]
-or
-\[C(t)=\left(\left\langle A(t)\cdot B(0)\right\rangle -\left\langle A(0)\cdot B(0)\right\rangle\right)\]
-These can be used to estimate statistical error, or to compute transport coefficients from Green-Kubo type formulas.
-A and B are observables, functions of the form f(sys::System,neighbors=nothing) which return types supporting the
-
-the output of A and B can be numeric or vectors (including vectors of SVectors, like the positions or velocities), in which case the products inside the brackets are dot products.
+\$\$ C(t)=\\left(\\left\\langle A(t)\\cdot B(0)\\right\\rangle -\\left\\langle A\\right\\rangle\\cdot \\left\\langle B\\right\\rangle\\right)\\left(\\sqrt{\\left\\langle |A|^2\\right\\rangle\\left\\langle |B|^2\\right\\rangle}\\right)^{-1}\$\$
+(normalized form), or
+\$\$C(t)=\\left(\\left\\langle A(t)\\cdot B(0)\\right\\rangle -\\left\\langle \\right\\rangle\\cdot \\left\\langle B\\right\\rangle\\right)\$\$
+(unnormalized form). These can be used to estimate statistical error, or to compute transport coefficients from Green-Kubo type formulas.
+A and B are observables, functions of the form f(sys::System,neighbors=nothing).    
+The return values of A and B can be of scalar or vector type (including vectors of `SVector`s, like positions or velocities), and must implement `dot`
 # Arguments
 - `TA::DataType`: The `DataType` returned by `A`, suppporting `zero(TA)`.
 - `observableA::Function`: The function corresponding to observable A.
@@ -320,9 +321,11 @@ the output of A and B can be numeric or vectors (including vectors of SVectors, 
 
  `n_correlation` should typically be chosen so that `dt*n_correlation>t_corr`,
   where `dt` is the simulation timestep and `t_corr` is the decorrelation time for the considered system and observables.
+
+  For the purpose of numerical stability, the logger internally records sums instead of running averages. The normalized and unnormalized form of the correlation function can be retrieved 
+ from a`logger::TimeCorrelationLogger` through accessing the `logger.normalized_correlations` and `logger.unnormalized_correlations` properties.
 """
 mutable struct TimeCorrelationLogger{T_A,T_A2,T_B,T_B2,T_AB,TF_A,TF_B}
-
     observableA::TF_A
     observableB::TF_B
 
@@ -331,7 +334,7 @@ mutable struct TimeCorrelationLogger{T_A,T_A2,T_B,T_B2,T_AB,TF_A,TF_B}
     history_A::CircularBuffer{T_A}
     history_B::CircularBuffer{T_B}
 
-    sum_offset_products::Vector{T_AB}
+    sum_offset_products::Vector{T_AB} 
 
     n_timesteps::Int64
 
@@ -342,30 +345,30 @@ mutable struct TimeCorrelationLogger{T_A,T_A2,T_B,T_B2,T_AB,TF_A,TF_B}
     sum_sq_B::T_B2
 end
 
-function TimeCorrelationLogger(TA::DataType,TB::DataType, observableA::T_FA, observableB::T_FB, observable_length::Integer,n_correlation::Integer) where {T_FA,T_FB}
-    ini_sum_A= (observable_length>1) ? zeros(TA,observable_length) : zero(TA)
-    ini_sum_B= (observable_length>1) ? zeros(TB,observable_length) : zero(TB)
-    
-    ini_sum_sq_A=dot(ini_sum_A,ini_sum_A)
-    ini_sum_sq_B=dot(ini_sum_B,ini_sum_B)
+function TimeCorrelationLogger(TA::DataType, TB::DataType, observableA::TF_A, observableB::TF_B, observable_length::Integer, n_correlation::Integer) where {TF_A,TF_B}
+    ini_sum_A = (observable_length > 1) ? zeros(TA, observable_length) : zero(TA)
+    ini_sum_B = (observable_length > 1) ? zeros(TB, observable_length) : zero(TB)
 
-    T_A=typeof(ini_sum_A)
-    T_A2=typeof(ini_sum_sq_A)
+    ini_sum_sq_A = dot(ini_sum_A, ini_sum_A)
+    ini_sum_sq_B = dot(ini_sum_B, ini_sum_B)
 
-    T_B=typeof(ini_sum_B)
-    T_B2=typeof(ini_sum_B)
+    T_A = typeof(ini_sum_A)
+    T_A2 = typeof(ini_sum_sq_A)
 
-    T_AB=typeof(zero(TA)*zero(TB))
-    
+    T_B = typeof(ini_sum_B)
+    T_B2 = typeof(ini_sum_sq_B)
+
+    T_AB = typeof(dot(zero(TA),zero(TB)))
+
     return TimeCorrelationLogger{T_A,T_A2,T_B,T_B2,T_AB,TF_A,TF_B}(observableA, observableB, n_correlation, CircularBuffer{T_A}(n_correlation), CircularBuffer{T_B}(n_correlation), zeros(T_AB, n_correlation), 0, ini_sum_A, ini_sum_B, ini_sum_sq_A, ini_sum_sq_B)
 
 end
 
-function log_property!(logger::TimeCorrelationLogger, s::System, neighbors=nothing, step_n::Integer=0;parallel::Bool=true)
+function log_property!(logger::TimeCorrelationLogger, s::System, neighbors=nothing, step_n::Integer=0; parallel::Bool=true)
 
     #compute observables
     A = logger.observableA(s, neighbors)
-    B = (logger.observableA!=logger.observableB) ? logger.observableB(s, neighbors) : A
+    B = (logger.observableA != logger.observableB) ? logger.observableB(s, neighbors) : A
 
     logger.n_timesteps += 1
 
@@ -383,8 +386,28 @@ function log_property!(logger::TimeCorrelationLogger, s::System, neighbors=nothi
 
     B1 = first(logger.history_B)
 
-    for i = 1:min(logger.n_correlation,logger.n_timesteps)
+    for i = 1:min(logger.n_correlation, logger.n_timesteps)
         logger.sum_offset_products[i] += dot(logger.history_A[i], B1)
     end
 
+end
+
+function Base.getproperty(logger::TimeCorrelationLogger,s::Symbol)
+    if (s != :normalized_correlations) && (s != :unnormalized_correlations)
+        return getfield(logger,s)
+    else
+        n_samps = getfield(logger, :n_timesteps)
+        C = zero(getfield(logger, :sum_offset_products))
+        C_bar = dot(getfield(logger, :sum_A) / n_samps, getfield(logger, :sum_B) / n_samps)
+        for i=1:getfield(logger, :n_correlation)
+            C[i]=getfield(logger, :sum_offset_products)[i] / (n_samps - i + 1)
+        end
+        C .-= C_bar
+        if s == :unnormalized_correlations
+            return C
+        else
+            denom = sqrt((getfield(logger, :sum_sq_A) / n_samps) * (getfield(logger, :sum_sq_B) / n_samps))
+            return C / denom
+        end
+    end
 end
