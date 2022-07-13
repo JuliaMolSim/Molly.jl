cf9c47cdef7b01a3744a94fa1e4738de44bbc0c3
Joe Greener <jgreener@hotmail.co.uk>
2022-07-13 19:26:15 +0100

    Andersen works with Zygote

--- src/coupling.jl
+++ src/coupling.jl
@@ -26,20 +26,31 @@ apply_coupling!(sys, sim, ::NoCoupling) = sys
     AndersenThermostat(temperature, coupling_const)
 
 Rescale random velocities according to the Andersen thermostat.
-Not currently compatible with automatic differentiation using Zygote.
 """
 struct AndersenThermostat{T, C}
     temperature::T
     coupling_const::C
 end
 
-function apply_coupling!(sys, sim, thermostat::AndersenThermostat)
+function apply_coupling!(sys::System{D, false}, sim, thermostat::AndersenThermostat) where D
     for i in 1:length(sys)
         if rand() < (sim.dt / thermostat.coupling_const)
             mass = sys.atoms[i].mass
             sys.velocities[i] = velocity(mass, thermostat.temperature, sys.k;
                                             dims=n_dimensions(sys))
         end
+    end
+    return sys
+end
+
+function apply_coupling!(sys::System{D, true, T}, sim, thermostat::AndersenThermostat) where {D, T}
+    atoms_to_bump = T.(rand(length(sys)) .< (sim.dt / thermostat.coupling_const))
+    atoms_to_leave = one(T) .- atoms_to_bump
+    vs = random_velocities(sys, thermostat.temperature)
+    if isa(sys.coords, CuArray)
+        sys.velocities = sys.velocities .* CuArray(atoms_to_leave) .+ vs .* CuArray(atoms_to_bump)
+    else
+        sys.velocities = sys.velocities .* atoms_to_leave .+ vs .* atoms_to_bump
     end
     return sys
 end
