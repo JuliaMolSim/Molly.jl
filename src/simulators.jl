5d4ad24964f0f0289531631ee7a55af842937e0e
JaydevSR <jaydevsr7701@gmail.com>
2022-07-22 12:40:09 +0530

    `TemperatureREMD` simulator update and doc

--- src/simulators.jl
+++ src/simulators.jl
@@ -439,79 +439,109 @@ function B_step!(s, dt_eff, acceleration_vector, compute_forces::Bool, n_threads
     s.velocities += dt_eff * remove_molar.(acceleration_vector)
 end
 
-struct TemperatureREMD{N, T, S, ST, ET, DT}
-    temps::StaticVector{N, T}
+"""
+    TemperatureREMD(; <keyword arguments>)
+
+A simulator for a parallel temperature replica exchange (TREX) simulation on a [`ReplicaSystem`](@ref). More information on this algorithm can be found in [Sugita Y., Okamoto Y. 1999](https://doi.org/10.1016/S0009-2614(99)01123-9).
+The corresponding [`ReplicaSystem`](@ref) should have the same number of replicas as the number of temperatures in the simulator.
+
+arguments:
+- `dt::S`: the time step of the simulation.
+- `temperatures::TP`: the temperatures corresponding to the replicas.
+- `simulators::ST`: individual simulators for simulating each replica.
+- `exchange_time::ET`: the time interval between replica exchange attempt.
+"""
+struct TemperatureREMD{N, T, S, DT, TP, ST, ET}
+    dt::DT
+    temperatures::TP
     simulators::ST
     exchange_time::ET
-    dt::DT
 end
 
 function TemperatureREMD(;
-                temps,
+                dt,
+                temperatures,
                 simulators,
                 exchange_time,
-                dt, kwargs...)
+                kwargs...)
     S = eltype(simulators)
-    T = eltype(temps)
-    N = length(temps)
-    ET = typeof(exchange_time)
+    T = eltype(temperatures)
+    N = length(temperatures)
     DT = typeof(dt)
-    temps = SA[temps...]
-    if length(simulators) != length(temps)
-        error("Number of simulators and temperatures values must be equal.")
+    TP = typeof(temperatures)
+    ET = typeof(exchange_time)
+    if length(simulators) != length(temperatures)
+        throw(ArgumentError("Number of temperatures must match number of simulators"))
+    end
+    if exchange_time <= dt
+        throw(ArgumentError("Exchange time must be greater than the time step"))
     end
     simulators = Tuple(simulators[i] for i in 1:N)
     ST = typeof(simulators)
     
-    return TemperatureREMD{N, T, S, ST, ET, DT}(temps, simulators, exchange_time, dt)
+    return TemperatureREMD{N, T, S, DT, TP, ST, ET}(temperatures, simulators, exchange_time, dt)
 end
 
-# simulate temperature replica exchange
 function simulate!(sys::ReplicaSystem,
                     sim::TemperatureREMD,
-                    n_steps::Integer;
-                    parallel::Bool=true)
+                    n_steps::Int;
+                    n_threads::Int=Threads.nthreads())
     if sys.n_replicas != length(sim.simulators)
         error("Number of replicas in ReplicaSystem and simulators in TemperatureREMD must match.")
     end
+
+    if n_threads > sys.n_replicas
+        thread_div = equal_parts(n_threads, sys.n_replicas)
+    else # pass 1 thread per replica
+        thread_div = equal_parts(sys.n_replicas, sys.n_replicas)
+    end
+
     # calculate n_cycles and n_steps_per_cycle from dt and exchange_time
     n_cycles = convert(Int64, (n_steps * sim.dt) ÷ sim.exchange_time)
     cycle_length = n_steps ÷ n_cycles
     remaining_steps = n_steps % n_cycles
 
+    # scale to correct temperatures
+    for i in eachindex(sys.replicas)
+        sys.replicas[i].velocities .*= sqrt(sim.temperatures[i] / temperature(sys.replicas[i]))
+    end
+
     for cycle=1:n_cycles
         @sync for idx in eachindex(sim.simulators)
-            if parallel
-                Threads.@spawn simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)
-            else
-                simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)
-            end
+            Threads.@spawn Molly.simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; n_threads=thread_div[idx])
         end
 
         if cycle != n_cycles
             n = rand(1:sys.n_replicas)
             m = mod(n, sys.n_replicas) + 1
-            β_n, β_m = 1/sim.temps[n], 1/sim.temps[m]
+            k_b = sys.k
+            β_n, β_m = 1/(k_b*sim.temperatures[n]), 1/(k_b*sim.temperatures[m])
             V_n, V_m = potential_energy(sys.replicas[n]), potential_energy(sys.replicas[m])
             Δ = ustrip((β_m - β_n)*(V_n - V_m))
             if Δ <= 0 || rand() < exp(-Δ)
-                n_exchanges += 1
                 sys.replicas[n].coords, sys.replicas[m].coords = sys.replicas[m].coords, sys.replicas[n].coords
                 # scale velocities
-                sys.replicas[n].velocities *= sqrt(β_n/β_m)
-                sys.replicas[m].velocities *= sqrt(β_m/β_n)
+                sys.replicas[n].velocities .*= sqrt(β_n/β_m)
+                sys.replicas[m].velocities .*= sqrt(β_m/β_n)
             end
         end
     end
+
     # run for remaining_steps (if >0) for all replicas
     if remaining_steps > 0
         @sync for idx in eachindex(sim.simulators)
-            if parallel
-                Threads.@spawn simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)
-            else
-                simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)
-            end
+            Threads.@spawn Molly.simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; n_threads=thread_div[idx])
         end
     end
     return sys
+end
+
+"""
+Returns k almost equal patitions of n
+"""
+@inline function equal_parts(n, k)
+    ndiv = n ÷ k
+    nrem = n % k
+    n_parts = ntuple(i -> (i <= nrem) ? ndiv+1 : ndiv, k)  # no allocation for k upto 10
+    return n_parts
 end
