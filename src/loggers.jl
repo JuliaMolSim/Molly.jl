b8cd2b1eb0d12ce5ba72b921a5f5bbd52c30847c
Joe Greener <jgreener@hotmail.co.uk>
2022-08-05 19:27:29 +0100

    ReplicaExchangeLogger constructor arg order

--- src/loggers.jl
+++ src/loggers.jl
@@ -476,39 +476,45 @@ function Base.show(io::IO, aol::AverageObservableLogger)
 end
 
 """
-    ReplicaExchangeLogger(n_steps::Int, T::DataType=DefaultFloat)
+    ReplicaExchangeLogger(n_steps)
+    ReplicaExchangeLogger(T, n_steps)
 
-A logger that records exchanges in a replica exchange simulation. The logged quantities include: number of exchange attempts (`n_attempts`),
-number of successful exchanges (`n_exchanges`), exchanged replica indices (`indices`), exchange steps (`steps`) and
-the value of Δ i.e. the argument of metropolis rate for the exchanges (`delta`).
+A logger that records exchanges in a replica exchange simulation.
+The logged quantities include the number of exchange attempts (`n_attempts`),
+number of successful exchanges (`n_exchanges`), exchanged replica indices (`indices`),
+exchange steps (`steps`) and the value of Δ i.e. the argument of metropolis rate for
+the exchanges (`delta`).
 """
 mutable struct ReplicaExchangeLogger{T}
     n_replicas::Int
     n_attempts::Int
     n_exchanges::Int
-    indices::Vector{Tuple{Int,Int}}
+    indices::Vector{Tuple{Int, Int}}
     steps::Vector{Int}
     delta::Vector{T}
     end_step::Int
 end
 
-ReplicaExchangeLogger(n_replicas::Int, T::DataType=DefaultFloat) = ReplicaExchangeLogger{T}(n_replicas, 0, 0, Tuple{Int, Int}[], Int[], T[], 0)
+function ReplicaExchangeLogger(T, n_replicas::Integer)
+    return ReplicaExchangeLogger{T}(n_replicas, 0, 0, Tuple{Int, Int}[], Int[], T[], 0)
+end
 
-function log_property!(
-                    rexl::ReplicaExchangeLogger,
-                    sys::ReplicaSystem,
-                    neighbors=nothing,
-                    step_n::Int=0;
-                    indices::Tuple{Int,Int},
-                    delta::Real,
-                    n_threads=Threads.nthreads())
+ReplicaExchangeLogger(n_replicas::Integer) = ReplicaExchangeLogger(DefaultFloat, n_replicas)
+
+function log_property!(rexl::ReplicaExchangeLogger,
+                       sys::ReplicaSystem,
+                       neighbors=nothing,
+                       step_n::Integer=0;
+                       indices,
+                       delta,
+                       n_threads::Integer=Threads.nthreads())
     push!(rexl.indices, indices)
-    push!(rexl.steps, step_n+rexl.end_step)
+    push!(rexl.steps, step_n + rexl.end_step)
     push!(rexl.delta, delta)
     rexl.n_exchanges += 1
 end
 
-function finish_logs!(rexl::ReplicaExchangeLogger; n_steps::Int=0, n_attempts::Int=0)
+function finish_logs!(rexl::ReplicaExchangeLogger; n_steps::Integer=0, n_attempts::Integer=0)
     rexl.end_step += n_steps
     rexl.n_attempts += n_attempts
 end
