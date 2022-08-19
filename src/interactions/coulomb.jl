48cc4a371d07af81bcb1522c947d382d3c2921a9
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-08-19 16:04:35 +0530

    another small fix

--- src/interactions/coulomb.jl
+++ src/interactions/coulomb.jl
@@ -127,7 +127,7 @@ V(r_{ij}) = \frac{q_i q_j}{4 \pi \varepsilon_0 (r_{ij}^6 + \alpha * sigma_{ij}^6
 ```
 
 Here, ``\\alpha``, ``\\lambda``, and ``\\p`` adjust the functional form of the soft core of the potential. For 
-`alpha=1` or `lambda=1` we get the standard Coulomb potential.
+`alpha=0` or `lambda=0` we get the standard Coulomb potential.
 """
 struct CoulombSoftCore{C, A, L, P, W, T, F, E} <: PairwiseInteraction
     cutoff::C
