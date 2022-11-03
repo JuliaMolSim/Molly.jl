b3d49894a886a30bd4628ef7c62cb9edf3c34c77
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-11-04 02:06:15 +0530

    add metropolis montecarlo simulator

---
+++ src/montecarlo.jl
@@ -0,0 +1,49 @@
+export MetropolisMonteCarlo,
+    random_uniform_displacement!,
+    random_normal_displacement!
+
+"""
+    MetropolisMonteCarlo(; <keyword arguments>)
+
+A Monte Carlo simulator for [`System`](@ref) that uses the Metropolis algorithm to sample the configuration space.
+
+# Arguments
+- `temperature::T`: The temperature of the system.
+- `log_interval::Int`: The interval at which to log the system state.
+- `trial_moves::UP`: A function that performs the trial moves.
+"""
+struct MetropolisMonteCarlo{T, M}
+    temperature::T
+    trial_moves::M
+    trial_args::Dict
+end
+
+function MetropolisMonteCarlo(;temperature::T,
+                                trial_moves::M,
+                                trial_args::Dict=Dict()) where {T, M}
+    return MetropolisMonteCarlo{T, M}(temperature, trial_moves, trial_args)
+end
+
+function Molly.simulate!(sys::System{D, G, T},
+                    sim::MetropolisMonteCarlo,
+                    n_steps::Int;
+                    n_threads::Int=Threads.nthreads()) where {D, G, T}
+    if dimension(sys.energy_units) == u"𝐋^2 * 𝐌 * 𝐍^-1 * 𝐓^-2"
+        k_b = sys.k * T(Unitful.Na) 
+    else
+        k_b = sys.k
+    end
+
+    for i in 1:n_steps
+        E_old = potential_energy(sys)
+        coords_old = copy(sys.coords)
+        sim.trial_moves(sys; sim.trial_args...)  # changes the coordinates of the system
+        E_new = potential_energy(sys)
+        ΔE = E_new - E_old
+        δ = ΔE / (k_b * sim.temperature)
+        if δ > 0 && rand() > exp(-δ)
+            sys.coords = coords_old
+        end
+        run_loggers!(sys, nothing, i; n_threads=n_threads)
+    end
+end
