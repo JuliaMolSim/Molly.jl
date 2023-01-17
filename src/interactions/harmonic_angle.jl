b06fb59089ee5089a9c8c897887dc26a8e674d77
Joe Greener <jgreener@hotmail.co.uk>
2023-01-17 15:01:43 +0000

    mention radians in docstrings

--- src/interactions/harmonic_angle.jl
+++ src/interactions/harmonic_angle.jl
@@ -4,6 +4,7 @@ export HarmonicAngle
     HarmonicAngle(; k, θ0)
 
 A harmonic bond angle between three atoms.
+`θ0` is in radians.
 The potential energy is defined as
 ```math
 V(\theta) = \frac{1}{2} k (\theta - \theta_0)^2
--- src/interactions/periodic_torsion.jl
+++ src/interactions/periodic_torsion.jl
@@ -4,6 +4,7 @@ export PeriodicTorsion
     PeriodicTorsion(; periodicities, phases, ks, proper)
 
 A periodic torsion angle between four atoms.
+`phases` are in radians.
 The potential energy is defined as
 ```math
 V(\phi) = \sum_{n=1}^N k_n (1 + \cos(n \phi - \phi_{s,n}))
