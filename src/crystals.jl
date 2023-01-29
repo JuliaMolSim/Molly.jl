36e9fd5caa29520f622fdaf00c06eafb5ff64cb6
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-01-29 12:28:04 -0500

    Update crystals.jl

--- src/crystals.jl
+++ src/crystals.jl
@@ -1,136 +1,180 @@
+#Only supports 3d crystals, cant make something like graphene
+
+#stop float promotion in primitive vectors??
+
 export
     SC,
     FCC,
     BCC
 
+#########
+# TYEPS #
+#########
 
-abstract type BravaisLattice end
+abstract type Lattice end
+abstract type BravaisLattice <: Lattice end #is this the right way to allow both LatticeWithBasis and a BravaisLattice to be passed to Crystal()?
 
-#Currently only single atom basis supported
-#How to incorperate lattice sites to allow for multi-atom crystals eg NaCl
-#stop float promotion in primitive vectors??
+#Allows construction of non-bravais lattices (e.g. diamond)
+struct LatticeWithBasis{B} <: Lattice
+    lattice::BravaisLattice
+    basis_vectors::SMatrix{B,3}
+end
 
-struct CubicBravaisLattice{PV,LC} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
+#Specific pattern at each bravais lattice point
+struct Basis{T}
+    position::SVector{3,T}
+    atom::Atom
+end
+
+struct Crystal
+    lattice::Lattice
+    basis::SVector{BasisAtom}
+end
+
+#####################################################
+
+struct CubicBravaisLattice{LC} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
 end
 
 function CubicBravaisLattice(a)
-    primitive_vectors = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0] .* a
-    return CubicBravaisLattice{typeof(primitive_vectors),typeof(lattice_constant)}(
-                primitive_vectors, lattice_constant)
+    return CubicBravaisLattice{typeof(lattice_constant)}(SVector(a,a,a))
 end
 
 #####################################################
 
-struct OrthorhombicBravaisLattice{PV,LC} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
-    b::LC
-    c::LC
+struct OrthorhombicBravaisLattice{LC} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
 end
 
 function OrthorhombicBravaisLattice(a,b,c)
-    primitive_vectors = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0] .* [a,b,c]
-    return OrthorhombicBravaisLattice{typeof(primitive_vectors),typeof(a)}(
-                    primitive_vectors, lattice_constant)
+    return OrthorhombicBravaisLattice{typeof(a)}(SVector(a,b,c))
 end
 
 #####################################################
 
-struct MonoclinicBravaisLattice{PV,LC,LA} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
-    b::LC
-    c::LC
-    β::LA
+struct MonoclinicBravaisLattice{LC,LA} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
+    lattice_angles::SVector{3,LA}
 end
 
 function MonoclinicBravaisLattice(a, b, c, β)
-    primitive_vectors = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0] .* [a,b,c]
-    rotateAboutX!(primitive_vectors, 90*u"°" - β)
-    return MonoclinicBravaisLattice{typeof(primitive_vectors),typeof(a),typeof(β)}(
-                    primitive_vectors, lattice_constant)
+    return MonoclinicBravaisLattice{typeof(a),typeof(β)}(SVector(a,b,c), SVector(90u"°", β, 90u"°"))
 end
 
 #####################################################
 
-struct TriclinicBravaisLattice{PV,LC,LA} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
-    b::LC
-    c::LC
-    α::LA
-    β::LA
-    γ::LA
+struct TriclinicBravaisLattice{LC,LA} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
+    lattice_angles::SVector{3,LA}
+end
+
+function TriclinicBravaisLattice(a, b, c, α, β, γ)
+    return TriclinicBravaisLattice{typeof(a),typeof(β)}(SVector(a,b,c), SVector(α, β, γ))
+end
+#####################################################
+
+struct TetragonalBravaisLattice{LC} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
+end
+
+function TetragonalBravaisLattice(a, c)
+    return TetragonalBravaisLattice{typeof(a)}(SVector(a,a,c))
 end
 
 #####################################################
 
-struct TetragonalBravaisLattice{PV,LC} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
-    c::LC
+struct RhombohedralBravaisLattice{LC,LA} <: BravaisLattice
+    lattice_constants::SVector{3,LC}
+    lattice_angles::SVector{3,LA}
 end
 
-function TetragonalBravaisLattice(a,c)
-    temp_lattice_constants = [a, a, c]
-    primitive_vectors = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0] .* temp_lattice_constants
-    return TetragonalBravaisLattice{typeof(primitive_vectors),typeof(a)}(
-                    primitive_vectors, lattice_constant)
-end
-
-#####################################################
-
-struct RhombohedralBravaisLattice{PV,LC,LA} <: BravaisLattice
-    primitive_vectors::PV
-    a::LC
-    α::LA
+function RhombohedralBravaisLattice(a, α)
+    return RhombohedralBravaisLattice{typeof(a),typeof(α)}(SVector(a,a,a),SVector(α, α, α))
 end
 
 #####################################################
 
 struct HexagonalBravaisLattice{PV,LC,LA} <: BravaisLattice
-    primitive_vectors::PV
+    edge_vectors::SVector{3, SVector{3, PV}}
     a::LC
     c::LC
     α::LA
 end
 
+function HexagonalBravaisLattice(a,c,γ)
+    return HexagonalBravaisLattice{typeof(a),typeof(γ)}(SVector(a,a,c),SVector(90u"°", 90u"°", γ))
+end
+
 #####################################################
 
-# All types of bravais lattice have a conventional cell
-function conventional(bl::BravaisLattice, num_unit_cells)
-    coords = 
-    boundary = 
-    return coords, boundary
+
+function Conventional(bl::BravaisLattice)
+    primitive_vectors = MMatrix{3,3}([1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0])
+    primitive_vectors .*= transpose(bl.lattice_constants)
+    return primitive_vectors
 end
 
 FC_supported_types = Union{CubicBravaisLattice, OrthorhombicBravaisLattice}
-function face_centered(bl::FC_supported_types, num_unit_cells)
-
-    return coords, boundary
-end
-
-Base_supported_types = Union{MonoclinicBravaisLattice, OrthorhombicBravaisLattice}
-function base_centered(bl::Base_supported_types, num_unit_cells)
-
-    return coords, boundary
+function FaceCentered(bl::FC_supported_types)
+    primitive_vectors = MMatrix{3,3}([0.0 0.5 0.5; 0.5 0.0 0.5; 0.5 0.5 0.0])
+    primitive_vectors .*= transpose(bl.lattice_constants)
+    return primitive_vectors
 end
 
 BC_supported_types = Union{CubicBravaisLattice, OrthorhombicBravaisLattice, TetragonalBravaisLattice}
-function body_centered(bl::BC_supported_types, num_unit_cells)
+function BodyCentered(bl::BC_supported_types)
+    primitive_vectors = MMatrix{3,3}([1.0 0.0 0.0; 0.0 1.0 0.0; 0.5  0.5  0.5])
+    primitive_vectors .*= transpose(bl.lattice_constants)
+    return primitive_vectors
+end
 
-    return coords, boundary
+Base_supported_types = Union{MonoclinicBravaisLattice, OrthorhombicBravaisLattice}
+function BaseCentered(bl::Base_supported_types)
+
+end
+
+#####################################################
+
+function BuildCrystal(crystal::Crystal, num_unit_cells)
+    # Build crystal geometry
+
+    return atoms, coords, boundary
 end
 
 
-#####################################################
-# Implement some crystals
+# These functions populate the conventional cell for the specific structures
 
-SC(a, num_unit_cells) = conventional(CubicBravaisLattice(a, num_unit_cells))
-FCC(a, num_unit_cells) = face_centered(CubicBravaisLattice(a, num_unit_cells))
-BCC(a, num_unit_cells) = body_centered(CubicBravaisLattice(a, num_unit_cells))
+# cubic_unit_cell = [0.0, 0.0, 0.0; ]
+# return coords, boundary, atoms
+
+
+
+# #####################################################
+# # Implement some crystals
+
+
+
+# # Cubic Crystals
+# Cubic(a, num_unit_cells) = conventional(CubicBravaisLattice(a), num_unit_cells)
+# FCC(a, num_unit_cells) = face_centered(CubicBravaisLattice(a), num_unit_cells)
+# BCC(a, num_unit_cells) = body_centered(CubicBravaisLattice(a), num_unit_cells)
+
+# # Triclinic Crystals
+# Triclinic(a,b,c,α,β,γ,num_unit_cells) = conventional(TriclinicBravaisLattice(a,b,c,α,β,γ),num_unit_cells)
+
+# #Monoclinic Crystals
+# Monoclinic() = conventional()
+# BaseCenteredMonoclinic() = base_centered()
+
+# #Orthorhombic Crystals
+# Orthorhombic() = conventional()
+# BaseCenteredOrthorhombic() = base_centered()
+# BodyCenteredOrthorhombic() = body_centered()
+# FaceCenteredOrthorhombic() = face_centered()
+
+
+#Multi-Atom Basis Crystals
 
 
 #####################################################
@@ -147,14 +191,3 @@ end
 function rotateAboutZ!(v, θ)
 
 end
-
-
-
-
-# function get_positions(ab::AtomicBasis)
-
-# end
-
-# struct AtomicBasis{} <: AbstractCrystal
-
-# end
