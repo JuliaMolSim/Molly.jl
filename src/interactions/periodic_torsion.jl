a9a6741dc4e3bd574835dd6d18442aa6486f7f7f
Joe Greener <jgreener@hotmail.co.uk>
2023-02-09 15:38:32 +0000

    GPU periodic torsion force to work with Enzyme

--- src/interactions/periodic_torsion.jl
+++ src/interactions/periodic_torsion.jl
@@ -50,19 +50,7 @@ function Base.:+(p1::PeriodicTorsion{N, T, E}, p2::PeriodicTorsion{N, T, E}) whe
     )
 end
 
-function periodic_torsion_force(periodicity, phase, k, ab, bc, cd, cross_ab_bc, cross_bc_cd,
-                                bc_norm, θ)
-    dEdθ = -k * periodicity * sin((periodicity * θ) - phase)
-    fi =  dEdθ * bc_norm * cross_ab_bc / dot(cross_ab_bc, cross_ab_bc)
-    fl = -dEdθ * bc_norm * cross_bc_cd / dot(cross_bc_cd, cross_bc_cd)
-    v = (dot(-ab, bc) / bc_norm^2) * fi - (dot(-cd, bc) / bc_norm^2) * fl
-    fj =  v - fi
-    fk = -v - fl
-    return SpecificForce4Atoms(fi, fj, fk, fl)
-end
-
-@inline @inbounds function force(d::PeriodicTorsion, coords_i, coords_j, coords_k,
-                                 coords_l, boundary)
+function periodic_torsion_vectors(coords_i, coords_j, coords_k, coords_l, boundary)
     ab = vector(coords_i, coords_j, boundary)
     bc = vector(coords_j, coords_k, boundary)
     cd = vector(coords_k, coords_l, boundary)
@@ -73,19 +61,59 @@ end
         ustrip(dot(cross_ab_bc × cross_bc_cd, bc / bc_norm)),
         ustrip(dot(cross_ab_bc, cross_bc_cd)),
     )
+    return ab, bc, cd, cross_ab_bc, cross_bc_cd, bc_norm, θ
+end
+
+function periodic_torsion_force(periodicity, phase, k, ab, bc, cd, cross_ab_bc, cross_bc_cd,
+                                bc_norm, θ)
+    dEdθ = -k * periodicity * sin((periodicity * θ) - phase)
+    fi =  dEdθ * bc_norm * cross_ab_bc / dot(cross_ab_bc, cross_ab_bc)
+    fl = -dEdθ * bc_norm * cross_bc_cd / dot(cross_bc_cd, cross_bc_cd)
+    v = (dot(-ab, bc) / bc_norm^2) * fi - (dot(-cd, bc) / bc_norm^2) * fl
+    fj =  v - fi
+    fk = -v - fl
+    return fi, fj, fk, fl
+end
+
+# The summation gives different errors with Enzyme on CPU and GPU
+#   so there are two similar implementations
+@inline @inbounds function force(d::PeriodicTorsion, coords_i, coords_j, coords_k,
+                                 coords_l, boundary)
+    ab, bc, cd, cross_ab_bc, cross_bc_cd, bc_norm, θ = periodic_torsion_vectors(
+                                        coords_i, coords_j, coords_k, coords_l, boundary)
     fs = sum(zip(d.periodicities, d.phases, d.ks)) do (periodicity, phase, k)
-        # In a function to work with Enzyme
-        return periodic_torsion_force(periodicity, phase, k, ab, bc, cd, cross_ab_bc, cross_bc_cd,
-                                      bc_norm, θ)
+        fi, fj, fk, fl = periodic_torsion_force(periodicity, phase, k, ab, bc, cd, cross_ab_bc,
+                                                cross_bc_cd, bc_norm, θ)
+        return SpecificForce4Atoms(fi, fj, fk, fl)
     end
     return fs
 end
 
-@inline @inbounds function potential_energy(d::PeriodicTorsion, coords_i, coords_j, coords_k,
-                                            coords_l, boundary)
+@inline @inbounds function force_gpu(d::PeriodicTorsion{N}, coords_i, coords_j, coords_k,
+                                     coords_l, boundary) where N
+    ab, bc, cd, cross_ab_bc, cross_bc_cd, bc_norm, θ = periodic_torsion_vectors(
+                                        coords_i, coords_j, coords_k, coords_l, boundary)
+    fi_sum, fj_sum, fk_sum, fl_sum = periodic_torsion_force(d.periodicities[1], d.phases[1],
+                                        d.ks[1], ab, bc, cd, cross_ab_bc, cross_bc_cd, bc_norm, θ)
+    for i in 2:N
+        fi, fj, fk, fl = periodic_torsion_force(d.periodicities[i], d.phases[i], d.ks[i], ab, bc,
+                                                cd, cross_ab_bc, cross_bc_cd, bc_norm, θ)
+        fi_sum += fi
+        fj_sum += fj
+        fk_sum += fk
+        fl_sum += fl
+    end
+    return SpecificForce4Atoms(fi_sum, fj_sum, fk_sum, fl_sum)
+end
+
+@inline @inbounds function potential_energy(d::PeriodicTorsion{N}, coords_i, coords_j, coords_k,
+                                            coords_l, boundary) where N
     θ = torsion_angle(coords_i, coords_j, coords_k, coords_l, boundary)
-    E = sum(zip(d.periodicities, d.phases, d.ks)) do (periodicity, phase, k)
-        return k + k * cos((periodicity * θ) - phase)
+    k1 = d.ks[1]
+    E = k1 + k1 * cos((d.periodicities[1] * θ) - d.phases[1])
+    for i in 2:N
+        k = d.ks[i]
+        E += k + k * cos((d.periodicities[i] * θ) - d.phases[i])
     end
     return E
 end
