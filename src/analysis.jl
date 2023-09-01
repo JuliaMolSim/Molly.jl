77f4296a4f35867dd4f08607c486c00d490dfce0
Joe Greener <jgreener@hotmail.co.uk>
2023-09-01 18:36:13 +0100

    fix doc formatting

--- src/analysis.jl
+++ src/analysis.jl
@@ -169,7 +169,7 @@ end
 
 Calculate the hydrodynamic radius of a set of coordinates.
 
-`R_{hyd}` is defined by
+``R_{hyd}`` is defined by
 ```math
 \frac{1}{R_{hyd}} = \frac{1}{2N^2}\sum_{i \neq j} \frac{1}{r_{ij}}
 ```
