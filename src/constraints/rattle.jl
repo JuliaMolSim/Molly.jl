3b67e23afe64c6debdeb743cb5e551c5d8b31836
ejmeitz <54505069+ejmeitz@users.noreply.github.com>
2024-02-05 08:11:02 -0500

    convert rattle to iterative

--- src/constraints/rattle.jl
+++ src/constraints/rattle.jl
@@ -9,42 +9,99 @@ apply_vel_correction!(vels, ca::SHAKE_RATTLE) = vels .+= ca.vel_storage
 function apply_velocity_constraints!(sys::System, constraint_algo::SHAKE_RATTLE;
      n_threads::Integer=Threads.nthreads())
 
-    Threads.@threads for group_id in 1:n_threads #& can only paralellize over independent clusters
-        for i in group_id:n_threads:length(sys.constraints)
-            RATTLE_update!(sys, sys.constraints[i])
-        end
-    end
+    # Threads.@threads for group_id in 1:n_threads #& can only paralellize over independent clusters
+    #     for i in group_id:n_threads:length(sys.constraints)
+    #         RATTLE_update!(sys, sys.constraints[i])
+    #     end
+    # end
+
+    RATTLE_updates!(sys, constraint_algo)
  
     return sys
 end
+
+function RATTLE_updates!(sys, ca::SHAKE_RATTLE)
+
+    converged = false
+
+    while !converged
+        for cluster in sys.constraints #& illegal to parallelize this
+            for constraint in cluster.constraints
+
+                # Index of atoms in bond k
+                k1, k2 = constraint.atom_idxs
+
+                # Inverse of masses of atoms in bond k
+                inv_m1 = 1/mass(sys.atoms[k1])
+                inv_m2 = 1/mass(sys.atoms[k2])
+
+                # Distance vector between the atoms after SHAKE constraint
+                r_k1k2 = vector(sys.coords[k2], sys.coords[k1], sys.boundary)
+
+                # Difference between unconstrainted velocities
+                v_k1k2 = sys.velocities[k2] .- sys.velocities[k1]
+
+                err = abs(dot(r_k1k2, v_k1k2))
+                if err > ca.vel_tolerance
+                    # Re-arrange constraint equation to solve for Lagrange multiplier
+                    # Technically this has a factor of dt which cancels out in the velocity update
+                    λₖ = -dot(r_k1k2,v_k1k2)/(dot(r_k1k2,r_k1k2)*(inv_m1 + inv_m2))
+
+                    # Correct velocities
+                    sys.velocities[k1] -= (inv_m1 .* λₖ .* r_k1k2)
+                    sys.velocities[k2] += (inv_m2 .* λₖ .* r_k1k2)
+                end
+
+            end
+        end
+
+        max_err = typemin(float_type(sys))*unit(sys.velocities[1][1])*unit(sys.coords[1][1])
+        for cluster in sys.constraints
+            for constraint in cluster.constraints
+                k1, k2 = constraint.atom_idxs
+                err = abs(dot(vector(sys.coords[k2], sys.coords[k1], sys.boundary), (sys.velocities[k2] .- sys.velocities[k1])))
+                if max_err < err
+                    max_err = err
+                end
+            end
+        end
+
+        if max_err < ca.vel_tolerance
+            converged = true
+        end
+
+    end
+
+end
+
 
 """
 RATTLE solution for a single distance constraint between atoms i and j,
 where atoms i and j do NOT participate in any other constraints.
 """
-function RATTLE_update!(sys, cluster::ConstraintCluster{1})
+# function RATTLE_update!(sys, cluster::ConstraintCluster{1})
 
-    constraint = cluster.constraints[1]
+#     constraint = cluster.constraints[1]
 
-    # Index of atoms in bond k
-    k1, k2 = constraint.atom_idxs
+#     # Index of atoms in bond k
+#     k1, k2 = constraint.atom_idxs
 
-    # Inverse of masses of atoms in bond k
-    inv_m1 = 1/mass(sys.atoms[k1])
-    inv_m2 = 1/mass(sys.atoms[k2])
+#     # Inverse of masses of atoms in bond k
+#     inv_m1 = 1/mass(sys.atoms[k1])
+#     inv_m2 = 1/mass(sys.atoms[k2])
 
-    # Distance vector between the atoms after SHAKE constraint
-    r_k1k2 = vector(sys.coords[k2], sys.coords[k1], sys.boundary)
+#     # Distance vector between the atoms after SHAKE constraint
+#     r_k1k2 = vector(sys.coords[k2], sys.coords[k1], sys.boundary)
 
-    # Difference between unconstrainted velocities
-    v_k1k2 = sys.velocities[k2] .- sys.velocities[k1]
+#     # Difference between unconstrainted velocities
+#     v_k1k2 = sys.velocities[k2] .- sys.velocities[k1]
 
-    # Re-arrange constraint equation to solve for Lagrange multiplier
-    # Technically this has a factor of dt which cancels out in the velocity update
-    λₖ = -dot(r_k1k2,v_k1k2)/(dot(r_k1k2,r_k1k2)*(inv_m1 + inv_m2))
+#     # Re-arrange constraint equation to solve for Lagrange multiplier
+#     # Technically this has a factor of dt which cancels out in the velocity update
+#     λₖ = -dot(r_k1k2,v_k1k2)/(dot(r_k1k2,r_k1k2)*(inv_m1 + inv_m2))
 
-    # Correct velocities
-    sys.velocities[k1] -= (inv_m1 .* λₖ .* r_k1k2)
-    sys.velocities[k2] += (inv_m2 .* λₖ .* r_k1k2)
+#     # Correct velocities
+#     sys.velocities[k1] -= (inv_m1 .* λₖ .* r_k1k2)
+#     sys.velocities[k2] += (inv_m2 .* λₖ .* r_k1k2)
 
-end
+# end
--- src/constraints/shake.jl
+++ src/constraints/shake.jl
@@ -18,16 +18,17 @@ system solved to satisfy the RATTLE algorithm.
 - init_posn_tol: Tolerance used when checking if system initial positions satisfy position constraints. 
     Default is `nothing.`
 """
-struct SHAKE_RATTLE{UC, VC, T, I} <: ConstraintAlgorithm
+struct SHAKE_RATTLE{UC, VC, D, V, I} <: ConstraintAlgorithm
     coord_storage::UC #Used as storage to avoid re-allocating arrays
     vel_storage::Union{VC, Nothing}
-    tolerance::T
+    dist_tolerance::D
+    vel_tolerance::V
     init_posn_tol::Union{I,Nothing}
 end
 
-function SHAKE_RATTLE(coord_storage, vel_storage; tolerance=1e-4, init_posn_tol = nothing)
-    return SHAKE_RATTLE{typeof(coord_storage), typeof(vel_storage), typeof(tolerance), typeof(init_posn_tol)}(
-        coord_storage, vel_storage, tolerance, init_posn_tol)
+function SHAKE_RATTLE(coord_storage, vel_storage; dist_tolerance=1e-4, vel_tolerance = 1e-4, init_posn_tol = nothing)
+    return SHAKE_RATTLE{typeof(coord_storage), typeof(vel_storage), typeof(dist_tolerance),  typeof(vel_tolerance), typeof(init_posn_tol)}(
+        coord_storage, vel_storage, dist_tolerance, vel_tolerance, init_posn_tol)
 end
 
 save_positions!(constraint_algo::SHAKE_RATTLE, c) = (constraint_algo.coord_storage .= c)
@@ -46,6 +47,69 @@ function apply_position_constraints!(sys::System, constraint_algo::SHAKE_RATTLE;
     return sys
 end
 
+
+
+function SHAKE_updates!(sys, ca::SHAKE_RATTLE)
+    converged = false
+
+    while !converged
+        for cluster in sys.constraints #& illegal to parallelize this
+            for constraint in cluster.constraints
+
+                k1, k2 = constraint.atom_idxs
+        
+                # Distance vector after unconstrained update (s)
+                s12 = vector(sys.coords[k2], sys.coords[k1], sys.boundary) #& extra allocation
+
+                # Distance vector between the atoms before unconstrained update (r)
+                r12 = vector(ca.coord_storage[k2], ca.coord_storage[k1], sys.boundary) #& extra allocation
+
+                if abs(norm(s12) - constraint.dist) > ca.dist_tolerance
+                    m1_inv = 1/mass(sys.atoms[k1])
+                    m2_inv = 1/mass(sys.atoms[k2])
+                    a = (m1_inv + m2_inv)^2 * norm(r12)^2 #* can remove sqrt in norm here
+                    b = -2 * (m1_inv + m2_inv) * dot(r12, s12)
+                    c = norm(s12)^2 - ((constraint.dist)^2) #* can remove sqrt in norm here
+                    D = (b^2 - 4*a*c)
+                    
+                    if ustrip(D) < 0.0
+                        @warn "SHAKE determinant negative, setting to 0.0"
+                        D = zero(D)
+                    end
+
+                    # Quadratic solution for g
+                    α1 = (-b + sqrt(D)) / (2*a)
+                    α2 = (-b - sqrt(D)) / (2*a)
+
+                    g = abs(α1) <= abs(α2) ? α1 : α2 #* why take smaller one?
+
+                    # Update positions
+                    δri1 = r12 .* (-g*m1_inv)
+                    δri2 = r12 .* (g*m2_inv)
+
+                    sys.coords[k1] += δri1
+                    sys.coords[k2] += δri2
+                end
+            end
+    
+        end
+        
+        max_err = typemin(float_type(sys))*unit(sys.coords[1][1])
+        for cluster in sys.constraints
+            for constraint in cluster.constraints
+                k1, k2 = constraint.atom_idxs
+                err = abs(norm(vector(sys.coords[k2], sys.coords[k1], sys.boundary)) - constraint.dist)
+                if max_err < err
+                    max_err = err
+                end
+            end
+        end
+
+        if max_err < ca.dist_tolerance
+            converged = true
+        end
+    end
+end
 
 # function SHAKE_update!(sys, ca::SHAKE_RATTLE, cluster::ConstraintCluster{1})
 
@@ -94,66 +158,3 @@ end
 #     # accels[k2] -= (lambda/m2).*r12
 
 # end
-
-
-function SHAKE_updates!(sys, ca::SHAKE_RATTLE)
-    converged = false
-
-    while !converged
-        for cluster in sys.constraints #& illegal to parallelize this
-            for constraint in cluster.constraints
-
-                k1, k2 = constraint.atom_idxs
-        
-                # Distance vector after unconstrained update (s)
-                s12 = vector(sys.coords[k2], sys.coords[k1], sys.boundary) #& extra allocation
-
-                # Distance vector between the atoms before unconstrained update (r)
-                r12 = vector(ca.coord_storage[k2], ca.coord_storage[k1], sys.boundary) #& extra allocation
-
-                if abs(norm(s12) - constraint.dist) > ca.tolerance
-                    m1_inv = 1/mass(sys.atoms[k1])
-                    m2_inv = 1/mass(sys.atoms[k2])
-                    a = (m1_inv + m2_inv)^2 * norm(r12)^2 #* can remove sqrt in norm here
-                    b = -2 * (m1_inv + m2_inv) * dot(r12, s12)
-                    c = norm(s12)^2 - ((constraint.dist)^2) #* can remove sqrt in norm here
-                    D = (b^2 - 4*a*c)
-                    
-                    if ustrip(D) < 0.0
-                        @warn "SHAKE determinant negative, setting to 0.0"
-                        D = zero(D)
-                    end
-
-                    # Quadratic solution for g
-                    α1 = (-b + sqrt(D)) / (2*a)
-                    α2 = (-b - sqrt(D)) / (2*a)
-
-                    g = abs(α1) <= abs(α2) ? α1 : α2 #* why take smaller one?
-
-                    # Update positions
-                    δri1 = r12 .* (-g*m1_inv)
-                    δri2 = r12 .* (g*m2_inv)
-
-                    sys.coords[k1] += δri1
-                    sys.coords[k2] += δri2
-                end
-            end
-    
-        end
-        
-        max_err = typemin(float_type(sys))*u"Å"
-        for cluster in sys.constraints
-            for constraint in cluster.constraints
-                k1, k2 = constraint.atom_idxs
-                err = norm(vector(sys.coords[k2], sys.coords[k1], sys.boundary)) - constraint.dist
-                if max_err < err
-                    max_err = err
-                end
-            end
-        end
-
-        if max_err < ca.tolerance
-            converged = true
-        end
-    end
-end
