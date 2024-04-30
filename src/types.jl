9ae2ff4911e0d71940281bdbdc4e1da38cfa301a
Joe Greener <jgreener@hotmail.co.uk>
2024-04-30 10:50:18 +0100

    allow f32 in MollyCalculator

--- src/types.jl
+++ src/types.jl
@@ -1243,9 +1243,9 @@ function MollyCalculator(;
                            force_units, energy_units, k)
 end
 
-# Doesn't work for Float32
 function AtomsCalculators.promote_force_type(::Any, calc::MollyCalculator)
-    return typeof(SVector(1.0, 1.0, 1.0) * calc.force_units)
+    T = typeof(ustrip(calc.k))
+    return typeof(ones(SVector{3, T}) * calc.force_units)
 end
 
 AtomsCalculators.@generate_interface function AtomsCalculators.forces(
