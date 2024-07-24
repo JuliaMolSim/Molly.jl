645585003e9efad2fdb4055be132566fd4c391f7
Joe Greener <jgreener@hotmail.co.uk>
2024-07-24 15:17:27 +0100

    make DMS test pass with Enzyme

--- ext/MollyEnzymeExt.jl
+++ ext/MollyEnzymeExt.jl
@@ -6,7 +6,9 @@ module MollyEnzymeExt
 using Molly
 using Enzyme
 
+EnzymeRules.inactive(::typeof(Molly.check_units), args...) = nothing
 EnzymeRules.inactive(::typeof(Molly.n_infinite_dims), args...) = nothing
+EnzymeRules.inactive(::typeof(random_velocity), args...) = nothing
 EnzymeRules.inactive(::typeof(random_velocities), args...) = nothing
 EnzymeRules.inactive(::typeof(random_velocities!), args...) = nothing
 EnzymeRules.inactive(::typeof(Molly.cuda_threads_blocks_pairwise), args...) = nothing
