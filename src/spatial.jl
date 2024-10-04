15258901df4be90314c2bc8951727c945aa6b02d
Joe Greener <jgreener@hotmail.co.uk>
2024-10-04 18:06:01 +0100

    dipole_moment function

--- src/spatial.jl
+++ src/spatial.jl
@@ -23,7 +23,8 @@ export
     virial,
     pressure,
     molecule_centers,
-    scale_coords!
+    scale_coords!,
+    dipole_moment
 
 """
     CubicBoundary(x, y, z)
@@ -942,3 +943,12 @@ function scale_coords!(sys, scale_factor; ignore_molecules=false)
     end
     return sys
 end
+
+"""
+    dipole_moment(sys)
+
+The dipole moment μ of a system.
+
+Requires the charges on the atoms to be set.
+"""
+dipole_moment(sys) = sum(sys.coords .* charges(sys))
