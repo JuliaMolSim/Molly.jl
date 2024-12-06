795a2bc61fdc73097c07fa7fbd064e81c3f1e7ed
Joe Greener <jgreener@hotmail.co.uk>
2024-12-06 11:48:35 +0000

    Langevin paper reference

--- src/simulators.jl
+++ src/simulators.jl
@@ -349,6 +349,7 @@ end
 
 The Langevin integrator, based on the Langevin Middle Integrator in OpenMM.
 
+See [Zhang et al. 2019](https://doi.org/10.1021/acs.jpca.9b02771).
 This is a leapfrog integrator, so the velocities are offset by half a time step
 behind the positions.
 
