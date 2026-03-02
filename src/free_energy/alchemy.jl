8276d590cd715f86fdc4ceb31fc485c2e731a20d
Alexandre Blanco <alexandrebg@mrc-lmb.cam.ac.uk>
2026-03-02 14:24:21 +0000

    Removed AbstractLambdaScheduler, compile-time type safety for lambda schedulers

--- src/free_energy/alchemy.jl
+++ src/free_energy/alchemy.jl
@@ -4,8 +4,6 @@ const CoreRole::AlchemicalRole   = 0x00
 const InsertRole::AlchemicalRole = 0x01
 const DeleteRole::AlchemicalRole = 0x02
 
-abstract type AbstractLambdaScheduler end
-
 #= 
 The logic found in this file is a reinterpretation of how OpenFE deals
 with alchemical transformations. The original logic can be found in:
@@ -16,7 +14,7 @@ https://github.com/OpenFreeEnergy/openfe/blob/main/src/openfe/protocols/openmm_r
 
 # Rule for combining roles during a pairwise interaction.
 # Dispatched on the scheduler to allow custom overriding by users.
-@inline function mix_roles(::AbstractLambdaScheduler, role_i::AlchemicalRole, role_j::AlchemicalRole)
+@inline function mix_roles(::Any, role_i::AlchemicalRole, role_j::AlchemicalRole)
     if role_i == InsertRole || role_j == InsertRole
         return InsertRole
     elseif role_i == DeleteRole || role_j == DeleteRole
@@ -26,13 +24,13 @@ https://github.com/OpenFreeEnergy/openfe/blob/main/src/openfe/protocols/openmm_r
     end
 end
 
-struct DefaultLambdaScheduler <: AbstractLambdaScheduler end
+struct DefaultLambdaScheduler end
 
 @inline function scale_sterics(::DefaultLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 2.0 * λ : 1.0)
+        return λ < T(0.5) ? T(2.0) * λ : T(1.0)
     elseif role == DeleteRole
-        return T(λ < 0.5 ? 0.0 : 2.0 * (λ - 0.5))
+        return λ < T(0.5) ? T(0.0) : T(2.0) * (λ - T(0.5))
     else
         return λ
     end
@@ -40,21 +38,21 @@ end
 
 @inline function scale_elec(::DefaultLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 0.0 : 2.0 * (λ - 0.5))
+        return T(λ < T(0.5) ? T(0.0) : T(2.0) * (λ - T(0.5)))
     elseif role == DeleteRole
-        return T(λ < 0.5 ? 2.0 * λ : 1.0)
+        return T(λ < T(0.5) ? T(2.0) * λ : T(1.0))
     else
         return λ
     end
 end
 
-struct NAMDLambdaScheduler <: AbstractLambdaScheduler end
+struct NAMDLambdaScheduler end
 
 @inline function scale_sterics(::NAMDLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < (2.0 / 3.0) ? (3.0 / 2.0) * λ : 1.0)
+        return T(λ < (T(2.0) / T(3.0)) ? (T(3.0) / T(2.0)) * λ : T(1.0))
     elseif role == DeleteRole
-        return T(λ < (1.0 / 3.0) ? 0.0 : (λ - (1.0 / 3.0)) * (3.0 / 2.0))
+        return T(λ < (T(1.0) / T(3.0)) ? T(0.0) : (λ - (T(1.0) / T(3.0))) * (T(3.0) / T(2.0)))
     else
         return λ
     end
@@ -62,21 +60,21 @@ end
 
 @inline function scale_elec(::NAMDLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 0.0 : 2.0 * (λ - 0.5))
+        return T(λ < T(0.5) ? T(0.0) : T(2.0) * (λ - T(0.5)))
     elseif role == DeleteRole
-        return T(λ < 0.5 ? 2.0 * λ : 1.0)
+        return T(λ < T(0.5) ? T(2.0) * λ : T(1.0))
     else
         return λ
     end
 end
 
-struct QuartersLambdaScheduler <: AbstractLambdaScheduler end
+struct QuartersLambdaScheduler end
 
 @inline function scale_sterics(::QuartersLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 0.0 : (λ > 0.75 ? 1.0 : 4.0 * (λ - 0.5)))
+        return λ < T(0.5) ? T(0.0) : (λ > T(0.75) ? T(1.0) : T(4.0) * (λ - T(0.5)))
     elseif role == DeleteRole
-        return T(λ < 0.25 ? 0.0 : (λ > 0.5 ? 1.0 : 4.0 * (λ - 0.25)))
+        return λ < T(0.25) ? T(0.0) : (λ > T(0.5) ? T(1.0) : T(4.0) * (λ - T(0.25)))
     else
         return λ
     end
@@ -84,21 +82,21 @@ end
 
 @inline function scale_elec(::QuartersLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.75 ? 0.0 : 4.0 * (λ - 0.75))
+        return λ < T(0.75) ? T(0.0) : T(4.0) * (λ - T(0.75))
     elseif role == DeleteRole
-        return T(λ < 0.25 ? 4.0 * λ : 1.0)
+        return λ < T(0.25) ? T(4.0) * λ : T(1.0)
     else
         return λ
     end
 end
 
-struct EleScaledLambdaScheduler <: AbstractLambdaScheduler end
+struct EleScaledLambdaScheduler end
 
 @inline function scale_sterics(::EleScaledLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 2.0 * λ : 1.0)
+        return λ < T(0.5) ? T(2.0) * λ : T(1.0)
     elseif role == DeleteRole
-        return T(λ < 0.5 ? 0.0 : 2.0 * (λ - 0.5))
+        return λ < T(0.5) ? T(0.0) : T(2.0) * (λ - T(0.5))
     else
         return λ
     end
@@ -106,9 +104,9 @@ end
 
 @inline function scale_elec(::EleScaledLambdaScheduler, λ::T, role::AlchemicalRole) where T
     if role == InsertRole
-        return T(λ < 0.5 ? 0.0 : sqrt(2.0 * (λ - 0.5)))
+        return λ < T(0.5) ? T(0.0) : sqrt(T(2.0) * (λ - T(0.5)))
     elseif role == DeleteRole
-        return T(λ < 0.5 ? (2.0 * λ)^2 : 1.0)
+        return λ < T(0.5) ? (T(2.0) * λ)^2 : T(1.0)
     else
         return λ
     end
