60d8f161535e650548412fe1f92d36340c5d5d53
Rainer Rodrigues <rainerrodrigues16@gmail.com>
2026-07-06 15:42:45 +0100

    feat: implement out-of-place device-agnostic system resizing for GPU compatibility

--- test/dynamic.jl
+++ test/dynamic.jl
@@ -1,11 +1,9 @@
 # test/dynamic.jl
-
 using Molly
 using Test
 using StaticArrays
 
 @testset "Dynamic System Resizing" begin
-    # Setting up a basic 3-atom system
     coords = [SVector(1.0, 1.0, 1.0), SVector(2.0, 2.0, 2.0), SVector(3.0, 3.0, 3.0)]
     velocities = [SVector(0.1, 0.1, 0.1), SVector(0.2, 0.2, 0.2), SVector(0.3, 0.3, 0.3)]
     atoms = [Atom(mass=1.0), Atom(mass=2.0), Atom(mass=3.0)]
@@ -16,27 +14,26 @@ using StaticArrays
         coords=coords, 
         velocities=velocities, 
         boundary=boundary,
-        energy_units=NoUnits, 
+        energy_units=NoUnits,
         force_units=NoUnits
     )
     
     @test length(sys.atoms) == 3
     
-    # Testing adding an atom
+    # Test adding an atom (OUT-OF-PLACE)
     new_atom = Atom(mass=4.0)
     new_coord = SVector(4.0, 4.0, 4.0)
     new_vel = SVector(0.4, 0.4, 0.4)
     
-    add_atom!(sys, new_atom, new_coord, new_vel)
+    sys = add_atom(sys, new_atom, new_coord, new_vel)
     
     @test length(sys.atoms) == 4
     @test sys.coords[4] == SVector(4.0, 4.0, 4.0)
     
-    # Testing O(1) swap-and-pop removal
-    # If we delete atom index 2, atom 4 should take its place
-    remove_atom!(sys, 2)
+    # Test standard ordered deletion (OUT-OF-PLACE)
+    sys = remove_atom(sys, 2)
     
     @test length(sys.atoms) == 3
-    @test sys.atoms[2].mass == 4.0               # Atom 4 moved to index 2
-    @test sys.coords[2] == SVector(4.0, 4.0, 4.0) # Atom 4 coords moved to index 2
+    @test sys.atoms[2].mass == 3.0
+    @test sys.coords[2] == SVector(3.0, 3.0, 3.0)
 end
